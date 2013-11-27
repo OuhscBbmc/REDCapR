88a3eb18e9fbc9a6ba4038104f005ad7aa7f5691
Will Beasley <wibeasley@insightresults.com>
2013-11-27 01:38:13 -0600

    Add refresh script

---
+++ utility_scripts/refresh.R
@@ -0,0 +1,25 @@
+rm(list=ls(all=TRUE))
+require(devtools)
+options(device = "windows") #http://support.rstudio.org/help/discussions/problems/80-error-in-function-only-one-rstudio-graphics-device-is-permitted
+
+devtools::check_doc() #Should return NULL
+#  system("R CMD Rd2pdf --force --output=./REDCapRDocumentationPeek.pdf ." )
+#
+devtools::run_examples(); dev.off() #This overwrites the NAMESPACE file too
+# devtools::run_examples(, "Ace.Rd")
+devtools::test()
+
+
+system("R CMD build --resave-data .") #Then move it up one directory.
+tarBallPattern <- "^REDCapR_.+\\.tar\\.gz$"
+file.copy(from=list.files(pattern=tarBallPattern), to="../", overwrite=TRUE)
+system(paste("R CMD check --as-cran", list.files(pattern=tarBallPattern, path="..//", full.names=TRUE)))
+unlink(list.files(pattern=tarBallPattern))
+unlink(list.files(pattern=tarBallPattern, path="..//", full.names=TRUE))
+unlink("REDCapR.Rcheck", recursive=T)
+# system("R CMD check --as-cran D:/Projects/RDev/NlsyLinksStaging/NlsyLinks_1.300.tar.gz")
+
+# devtools::build_win()
+devtools::revdep_check(pkg="NlsyLinks", recursive=TRUE)
+# devtools::revdep_check(pkg="Wats", recursive=TRUE)
+# devtools::release(check=FALSE) #Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
