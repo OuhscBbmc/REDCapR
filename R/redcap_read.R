85824f5186266e8e610020a2f8943bc8b07d72ff
Will Beasley <wibeasley@insightresults.com>
2013-11-27 22:38:05 -0600

    started documentation for read function

--- R/redcap_read.R
+++ R/redcap_read.R
@@ -1,3 +1,53 @@
+#' @name redcap_read
+#' @export redcap_read
+#' 
+#' @title Read records from a REDCap project
+#' 
+#' @usage redcap_read( redcap_uri, token, records=NULL, records_collapsed=NULL, fields=NULL, fields_collapsed=NULL, verbose=TRUE )
+#' 
+#' @description This function uses REDCap's \href{https://iwg.devguard.com/trac/redcap/wiki/ApiExamples}{API}
+#' to select and return data.
+#' 
+#' @param redcap_uri The URI of your REDCap project (\emph{eg}, https://ouhsc.edu/redcap).  Required.
+#' @param token The user-specific string that serves as the password for a project.  Required.
+#' @param records An array, where each element corresponds to the ID of a desired record.  Optional.
+#' @param records_collapsed A single string, where the desired ID values are separated by commas.  Optional.
+#' @param fields An array, where each element corresponds a desired project field.  Optional.
+#' @param records_collapsed A single string, where the desired field names are separated by commas.  Optional.
+#' @param verbose A boolean value indicating if \code{message}s should be printed to the R console during the operation.  Optional.
+#' 
+#' @return Currently, a list is returned with the arguments \code{ASquared},
+#' \code{CSquared}, \code{ESquared}, and \code{RowCount}.  In the future, this
+#' may be changed to an \code{S4} class.
+#' 
+#' @details 
+#' The \code{AceUnivariate} function is a wrapper that calls
+#' \code{DeFriesFulkerMethod1} or \code{DeFriesFulkerMethod3}.  Future
+#' versions will incorporate methods that use latent variable models.
+#' 
+#' @author Will Beasley
+#' @references The `API Examples' page on the REDCap wiki (\url{https://iwg.devguard.com/trac/redcap/wiki/ApiExamples}). A user account is required.
+#' @examples 
+#' \dontrun{
+#' library(REDCapR) #Load the package into the current R session.
+#' 
+#' #Return all records and all variables.
+#' ds_all_rows_all_fields <- redcap_read(redcap_uri=redcapUri, token=token)$data
+#' 
+#' #Return only records with IDs of 101, 102, 103, and 104
+#' desired_records <- c(101, 102, 103, 104)
+#' ds_some_rows <- redcap_read(redcap_uri=redcapUri, token=token, records=desired_records)$data
+#' 
+#' #Return only records with IDs of 101, 102, 103, and 104 (alternate way)
+#' desired_records <- "101, 102, 103, 104"
+#' ds_some_rows <- redcap_read(redcap_uri=redcapUri, token=token, records_collapsed=desired_records)$data
+#' 
+#' dsCall1 <- redcap_read(redcap_uri=redcapUri, token=token, fields=initialFields )$data
+#' 
+#' initialFields <- c("recruitid", "call1_recruiter", "call1_start", "call1_outcome", "inactive_record", "inactivated_date")
+#' dsCall1 <- redcap_read(redcap_uri=redcapUri, token=token, fields=initialFields )$data
+#' }
+
 redcap_read <- function( redcap_uri, token, records=NULL, records_collapsed=NULL, fields=NULL, fields_collapsed=NULL, verbose=TRUE ) {
   #TODO: unaswered verbose parameter pulls from getOption("verbose")
   start_time <- Sys.time()
