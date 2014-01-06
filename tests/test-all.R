4e943ad6d9dd0da07d7f8e21cf49d35ce9cebd8c
Will Beasley <wibeasley@hotmail.com>
2014-01-05 19:38:01 -0600

    going back to the recommended way

--- tests/test-all.R
+++ tests/test-all.R
@@ -6,12 +6,9 @@ testthatVersion <- packages[packages[, 1]=="testthat", "Version"]
 message("testthat package version: ", testthatVersion)
 
 if(  testthatVersion >= "0.8" ) {
-#   testthat::test_check("REDCapR")
-#   devtools::test()
-  testthat::test_package("REDCapR")
+  testthat::test_check("REDCapR")
 }
 rm(packages)
 
-
-#
-
+#   devtools::test()
+#   testthat::test_package("REDCapR")
