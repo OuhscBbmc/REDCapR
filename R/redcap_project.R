3b137e8971b25d0d44a830a2535adebc4543a5a5
Will Beasley <wibeasley@hotmail.com>
2014-03-14 10:10:25 -0500

    Reverting to stock roxygenize RC documentation for now
    It sounds like this will change soon
    https://github.com/klutometis/roxygen/issues/215

--- R/redcap_project.R
+++ R/redcap_project.R
@@ -19,7 +19,7 @@
 ##' uri <- "https://bbmc.ouhsc.edu/redcap/api/"
 ##' token <- "9A81268476645C4E5F03428B8AC3AA7B"
 ##' project <- redcap_project$new(redcap_uri=uri, token=token)
-##' project$read()
+##' ds <- project$read()
 
 redcap_project <- setRefClass(
   Class = "redcap_project",
