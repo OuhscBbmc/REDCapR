d95fcdb6aea09c74a9bb152a0d41d738ab4529b9
Will Beasley <wibeasley@hotmail.com>
2014-09-03 21:16:31 -0500

    Moving tests to ./tests/testthat/

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,16 +1,20 @@
-# library(testthat)
+#Modeled after the R6 testing structure: https://github.com/wch/R6/blob/master/tests/testthat.R
+library(testthat)
 library(REDCapR)
 
-packages <- utils::installed.packages()
-testthatVersion <- packages[packages[, 1]=="testthat", "Version"]
-message("testthat package version: ", testthatVersion)
+testthat::test_check("REDCapR")
+# test_results <- devtools::test()
 
-if(  testthatVersion >= "0.8" ) {
-#   testthat::test_check("REDCapR")
-  system.time(
-    testthat::test_package("REDCapR")
-  )
-}
-rm(packages)
+# packages <- utils::installed.packages()
+# testthatVersion <- packages[packages[, 1]=="testthat", "Version"]
+# message("testthat package version: ", testthatVersion)
+# 
+# if(  testthatVersion >= "0.8" ) {
+# #   testthat::test_check("REDCapR")
+#   system.time(
+#     testthat::test_package("REDCapR")
+#   )
+# }
+# rm(packages)
 
 #   devtools::test()
