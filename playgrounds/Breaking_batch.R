7f3aaca71016539317ed36adf63305248bb215d3
ARPeters <andrew.r.peters.42@gmail.com>
2014-09-17 12:17:28 -0500

    Testing batch function

--- playgrounds/Breaking_batch.R
+++ playgrounds/Breaking_batch.R
@@ -5,15 +5,15 @@ dummy_data<-redcap_read_oneshot(redcap_uri=uri, token=token)$data
 
 #testing create_batch_glossary function
 
-create_batch_glossary(nrow(dummy_data), batch_size=2)
 #breaks 5 rows into two batches of 2 and one of 1
+create_batch_glossary(nrow(dummy_data), batch_size=2)
 
-create_batch_glossary(nrow(dummy_data), batch_size=6)
 #creates one batch of 5, as we do not have enough rows to spill over to new batch
+create_batch_glossary(nrow(dummy_data), batch_size=6)
 
-create_batch_glossary(nrow(dummy_data), batch_size=1)
 #divides five rows into five batches of one row each.
 # I would be verys surprised if this turned out to be helpful to anyone.
+create_batch_glossary(nrow(dummy_data), batch_size=1)
 
-create_batch_glossary(nrow(dummy_data), batch_size=0)
 #returns an error message.
+create_batch_glossary(nrow(dummy_data), batch_size=0)
