9e3c50e7f27864f2187d5ccff9bde9e69468e0ac
Will Beasley <wibeasley@hotmail.com>
2015-03-08 18:40:27 -0500

    AppVeyor 1st try

--- utility_scripts/install_from_github.R
+++ utility_scripts/install_from_github.R
@@ -1,4 +1,3 @@
 #These two lines will install the development version of REDCapR from GitHub.
 install.packages("devtools")
-devtools::install_github(repo="OuhscBbmc/REDCapR") #Either version works.
-# devtools::install_github(repo="REDCapR", username="OuhscBbmc")
+devtools::install_github(repo="OuhscBbmc/REDCapR")
