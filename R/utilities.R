be3f906b4a815f690020d965364bd4c22919ddd4
Will Beasley <wibeasley@hotmail.com>
2015-03-08 16:12:42 -0500

    testing replace_nas_with_explicit

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,5 @@
-#' @name replace_nas_with_factor_level
-#' @export replace_nas_with_factor_level
+#' @name replace_nas_with_explicit
+## I'm intentionally not exporting this function.
 #' 
 #' @title Create explicit factor level for missing values.
 #'  
@@ -8,11 +8,14 @@
 #' 
 #' @param scores An array of values, ideally either factor or character. Required
 #' @param new_na_label The factor label assigned to the missing value.  Defaults to \code{Unknown}.
+#' @param create_factor Converts \code{scores} into a factor, if it isn't one already.   Defaults to \code{FALSE}.
 #' @param add_unknown_level Should a new factor level be created?  (Specify \code{TRUE} if it already exists.)   Defaults to \code{FALSE}.
 #' 
 #' @return An array of values, where the \code{NA} values are now a factor level, with the label specified by the \code{new_na_label} value.
 #' 
 #' @note
+#' The \code{create_factor} parameter is respected only if \code{scores} isn't already a factor.  Otherwise, levels without any values would be lost.
+#'
 #' A \code{stop} error will be thrown if the operation fails to convert all the \code{NA} values.
 #' 
 #' @author Will Beasley
@@ -20,15 +23,22 @@
 #' @examples
 #' library(REDCapR) #Load the package into the current R session.
 
-replace_nas_with_factor_level <- function( scores, new_na_label="Unknown", add_unknown_level=FALSE) {
+replace_nas_with_explicit <- function( scores, new_na_label="Unknown", create_factor=FALSE, add_unknown_level=FALSE) {
+  
+  if( create_factor & !is.factor(scores) ) {
+    scores <- factor(scores)
+  }
   
   if( add_unknown_level ) {
+    if( !is.factor(scores) )
+      stop("The `replace_nas_with_explicit()` function cannot add a level to `scores` when it is not a factor.  Consider setting `create_factor=TRUE`.")
+    
     levels(scores) <- c(levels(scores), new_na_label)  #Add a new level called 'Unknown'
   }
   
   scores[is.na(scores)] <- new_na_label #"Unknown"
   
-  if(any(is.na(scores))) 
+  if( any(is.na(scores)) ) 
     stop("The reassigned factor variable should not have any NA values.")
   
   return( scores )
--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -5,27 +5,70 @@ context("Replace NAs")
 ###########
 
 
-# test_that("replace_nas_standard", {
-#   a <- letters
-#   missing_indices <- c(3, 6, 8, 25)
-#   a[missing_indices] <- NA_character_
-#   
-#   expected <- c("a", "b", "Unknown", "d", "e", "Unknown", "g", "Unknown", "i", "j", "k", "l", "m", 
-#                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "Unknown", "z")
-#   
-#   a <- REDCapR::replace_nas_with_factor_level(a)
-#   expect_equal(a, expected)
-#   
-#   batchSize <- 100
-#   expectedGlossaryCount <- 1
-#   
-#   dsResult <- REDCapR::create_batch_glossary(row_count=rowCount, batch_size=batchSize) # dput(dsResult)
-#   dsExpected <- structure(list(id = 1L, start_index = 1, stop_index = 100, index_pretty = "1", 
-#                                start_index_pretty = "001", stop_index_pretty = "100", label = "1_001_100"), .Names = c("id", 
-#                                                                                                                        "start_index", "stop_index", "index_pretty", "start_index_pretty", 
-#                                                                                                                        "stop_index_pretty", "label"), row.names = c(NA, -1L), class = "data.frame")
-#   
-#   expect_equal(object=nrow(dsResult), expected=expectedGlossaryCount, info="The number of batches should be correct.")
-#   expect_equal(object=colnames(dsResult), expected=ExpectedColumnNames, info="The column namesshould be correct.")
-#   expect_equal(object=dsResult, expected=dsExpected, info="The returned batch glossary should be correct.")
-# })
+test_that("replace_nas_character_standard", {
+  a <- letters
+  missing_indices <- c(3, 6, 8, 25)
+  a[missing_indices] <- NA_character_
+  
+  expected <- c("a", "b", "Unknown", "d", "e", "Unknown", "g", "Unknown", "i", "j", "k", "l", "m", 
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "Unknown", "z")
+  
+  a <- REDCapR:::replace_nas_with_explicit(a)
+  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_equal(class(a), "character", "The returned array should remain a character.")
+})
+
+
+test_that("replace_nas_factor_standard", {
+  a <- factor(letters, levels=letters)
+  missing_indices <- c(3, 6, 8, 25)
+  a[missing_indices] <- NA_character_
+  
+  expected <- structure(c(1L, 2L, 27L, 4L, 5L, 27L, 7L, 27L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L, 19L, 20L, 21L, 22L, 23L, 24L, 27L, 26L), 
+                        .Label = c(letters, "Unknown"), 
+                        class = "factor")
+  
+  a <- REDCapR:::replace_nas_with_explicit(a, create_factor=F, add_unknown_level=T)
+  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_equal(class(a), "factor", "The returned array should be a converted factor.")
+})
+test_that("replace_nas_factor_create_not_existing", {
+  a <- letters
+  missing_indices <- c(3, 6, 8, 25)
+  a[missing_indices] <- NA_character_
+  
+  expected <- structure(c(1L, 2L, 23L, 3L, 4L, 23L, 5L, 23L, 6L, 7L, 8L, 9L,  10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L, 19L, 20L, 21L, 23L,  22L), 
+                        .Label = c("a", "b", "d", "e", "g", "i", "j", "k", "l",  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z", "Unknown"), 
+                        class = "factor")
+  
+  a <- REDCapR:::replace_nas_with_explicit(a, create_factor=T, add_unknown_level=T)
+  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_equal(class(a), "factor", "The returned array should be a converted factor.")
+})
+test_that("replace_nas_factor_create_already_existing", {
+  a <- factor(letters, levels=letters, labels=letters)
+  missing_indices <- c(3, 6, 8, 25)
+  a[missing_indices] <- NA_character_
+  
+  expected <- structure(c(1L, 2L, 27L, 4L, 5L, 27L, 7L, 27L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L, 19L, 20L, 21L, 22L, 23L, 24L, 27L, 26L), 
+                        .Label = c(letters, "Unknown"), 
+                        class = "factor")
+  
+  a <- REDCapR:::replace_nas_with_explicit(a, create_factor=T, add_unknown_level=T)
+  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_equal(class(a), "factor", "The returned array should be a converted factor.")
+})
+
+test_that("replace_nas_factor_not_yet", {
+  a <- letters
+  missing_indices <- c(3, 6, 8, 25)
+  a[missing_indices] <- NA_character_
+  
+  expected <- c("a", "b", "Unknown", "d", "e", "Unknown", "g", "Unknown", "i", "j", "k", "l", "m", 
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "Unknown", "z")
+  
+  expect_error(
+    REDCapR:::replace_nas_with_explicit(a, add_unknown_level=T)
+    , regexp = "The `replace_nas_with_explicit\\(\\)` function cannot add a level to `scores` when it is not a factor\\.  Consider setting `create_factor=TRUE`\\."
+  )
+})
