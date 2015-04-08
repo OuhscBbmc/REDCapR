b4a7a2651382aa69c65670aa1a14ba665aca9c2a
Will Beasley <wibeasley@hotmail.com>
2015-04-08 10:33:54 -0500

    testing w/ real REDCap dataset

--- tests/testthat/test-read_batch_longitudinal.R
+++ tests/testthat/test-read_batch_longitudinal.R
@@ -4,8 +4,9 @@ library(testthat)
 context("Read Batch - Longitudinal")
 ###########
 uri <- "https://bbmc.ouhsc.edu/redcap/api/"
-token <- "0434F0E9CF53ED0587847AB6E51DE762" #For `UnitTestPhiFree` account on pid=153.
+token <- "0434F0E9CF53ED0587847AB6E51DE762" #For `UnitTestPhiFree` account on pid=212.
 project <- redcap_project$new(redcap_uri=uri, token=token)
+directory_relative <- "test_data/project_longitudinal/expected"
 
 test_that("Smoke Test", {  
   testthat::skip_on_cran()
@@ -31,37 +32,38 @@ test_that("Smoke Test", {
   )
 })
 
-# test_that("All Records -Default", {   
-#   testthat::skip_on_cran()
-#   path_expected <- "./inst/test_data/project_longitudinal/expected/default.rds"
-#   expected_outcome_message <- "\\d+ records and \\d+ columns were read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\."
-#   
-#   expect_message(
-#     returned_object1 <- redcap_read(redcap_uri=uri, token=token, verbose=T),
-#     regexp = expected_outcome_message
-#   )
-#   
-#   # saveRDS(returned_object1$data, file=path_expected, ascii=F)#, compress="xz")
-#   # dump("returned_object1$data", file=path_expected)
-#   expected_data_frame <- readRDS(path_expected)
-#   # expected_data_frame <- eval(parse(path_expected), enclos = new.env()) #dput(returned_object1$data, file=path_expected)
-#   expect_equal(returned_object1$data, expected=expected_data_frame, label="The returned data.frame should be correct") # 
-#   
-#   expect_true(returned_object1$success)
-#   expect_match(returned_object1$status_codes, regexp="200", perl=TRUE)
-#   # expect_match(returned_object1$status_messages, regexp="OK", perl=TRUE)
-#   expect_true(returned_object1$records_collapsed=="", "A subset of records was not requested.")
-#   expect_true(returned_object1$fields_collapsed=="", "A subset of fields was not requested.")
-#   expect_match(returned_object1$outcome_messages, regexp=expected_outcome_message, perl=TRUE)
-# })
-test_that("SO example for data.frame retreival", {   
-  # path_relative <- "inst/test_data/project_longitudinal/expected/dummy.rds"
-  # path_qualified <- base::system.file(path_relative, package="REDCapR")
-  path_qualified <- base::file.path(devtools::inst(name="REDCapR"), "test_data/project_longitudinal/expected/dummy.rds")
+test_that("All Records -Default", {   
+  testthat::skip_on_cran()  
   
-  actual <- data.frame(a=1:5, b=6:10) #saveRDS(actual, file=path_relative)
-  # warning(getwd())
-  # browser()
+  file_name <- "default.rds"
+  path_qualified <- base::file.path(devtools::inst(name="REDCapR"), directory_relative, file_name)
+  
+  expected_outcome_message <- "\\d+ records and \\d+ columns were read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\."
+  expect_message(
+    returned_object1 <- redcap_read(redcap_uri=uri, token=token, verbose=T),
+    regexp = expected_outcome_message
+  )
+  
+  # saveRDS(returned_object1$data, file.path("./inst", directory_relative, file_name), compress="xz")
+  expected_data_frame <- readRDS(path_qualified)
+  # expected_data_frame <- eval(parse(path_expected), enclos = new.env()) #dput(returned_object1$data, file=path_expected)
+  expect_equal(returned_object1$data, expected=expected_data_frame, label="The returned data.frame should be correct")
+  
+  expect_true(returned_object1$success)
+  expect_match(returned_object1$status_codes, regexp="200", perl=TRUE)
+  # expect_match(returned_object1$status_messages, regexp="OK", perl=TRUE)
+  expect_true(returned_object1$records_collapsed=="", "A subset of records was not requested.")
+  expect_true(returned_object1$fields_collapsed=="", "A subset of fields was not requested.")
+  expect_match(returned_object1$outcome_messages, regexp=expected_outcome_message, perl=TRUE)
+})
+
+
+test_that("SO example for data.frame retreival", {   
+  file_name <- "dummy.rds"
+  path_qualified <- base::file.path(devtools::inst(name="REDCapR"), directory_relative, file_name)
+  
+  actual <- data.frame(a=1:5, b=6:10) # saveRDS(actual, file.path("./inst", directory_relative, file_name))
+  # browser() #getwd()
   expect_true(file.exists(path_qualified), "The saved data.frame should be retrieved from disk.")
   expected <- readRDS(path_qualified)
   expect_equal(actual, expected, label="The returned data.frame should be correct")
