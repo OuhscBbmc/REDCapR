ed343788d29695c1c69dd1e85aaa1dc37525cd4c
Will Beasley <wibeasley@hotmail.com>
2015-05-09 22:01:37 -0500

    renaming utility directory

--- utility/install_packages.R
+++ utility/install_packages.R
@@ -1,17 +1,17 @@
-#This code checks the user's installed packages against the packages listed in `./UtilityScripts/PackageDependencyList.csv`.
+#This code checks the user's installed packages against the packages listed in `./utility/package_dependency_list.csv`.
 #   These are necessary for the repository's R code to be fully operational. 
 #   CRAN packages are installed only if they're not already; then they're updated if available.
 #   GitHub packages are installed regardless if they're already installed.
-#If anyone encounters a package that should be on there, please add it to `./UtilityScripts/PackageDependencyList.csv`
+#If anyone encounters a package that should be on there, please add it to `./utility/package_dependency_list.csv`
 
 #Clear memory from previous runs.
 base::rm(list=base::ls(all=TRUE))
 
 #####################################
 ## @knitr DeclareGlobals
-pathCsv <- './utility_scripts/PackageDependencyList.csv'
+pathCsv <- './utility/package_dependency_list.csv'
 
-if( !file.exists(pathCsv)) 
+if( !file.exists(pathCsv) ) 
   base::stop("The path `", pathCsv, "` was not found.  Make sure the working directory is set to the root of the repository.")
 #####################################
 ## @knitr LoadDatasets
@@ -30,7 +30,7 @@ for( packageName in dsInstallFromCran$PackageName ) {
   available <- base::require(packageName, character.only=TRUE) #Loads the packages, and indicates if it's available
   if( !available ) {
     utils::install.packages(packageName, dependencies=TRUE)
-    base::require( packageName, character.only=TRUE)
+    base::require(packageName, character.only=TRUE)
   }
 }
 rm(dsInstallFromCran, packageName, available)
@@ -60,10 +60,3 @@ for( i in base::seq_len(base::nrow(dsInstallFromGitHub)) ) {
 }
 
 base::rm(dsInstallFromGitHub, repositoryName, username, i)
-
-#There will be a warning message for every  package that's called but not installed.  It will look like:
-#    Warning message:
-#        In library(package, lib.loc = lib.loc, character.only = TRUE, logical.return = TRUE,  :
-#        there is no package called 'bootstrap'
-#If you see the message (either in here or in another piece of the project's code),
-#   then run this again to make sure everything is installed.  You shouldn't get a warning again.
