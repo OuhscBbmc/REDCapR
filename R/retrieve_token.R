5d4a7d3841822f7d5788d51f93e72496f5eedea8
Will Beasley <wibeasley@hotmail.com>
2015-05-10 18:36:04 -0500

    testing bad `retrieve_token_mssql` parameters

--- R/retrieve_token.R
+++ R/retrieve_token.R
@@ -69,21 +69,29 @@ retrieve_token_mssql <- function(
   field_name_token = "Token"
   ) {
 
-  if( !requireNamespace("RODBC", quietly=TRUE) ) stop("The function REDCapR::retrieve_token_mssql() cannot run if the `RODBC` package is not installed.  Please install it and try again.")
+  if( !requireNamespace("RODBC", quietly=TRUE) ) 
+    stop("The function REDCapR::retrieve_token_mssql() cannot run if the `RODBC` package is not installed.  Please install it and try again.")
 
   regex_pattern_1 <- "^*[a-zA-Z0-9_]*$"
   regex_pattern_2 <- "^\\[*[a-zA-Z0-9_]*\\]*$"
   regex_pattern_3 <- "^@*[a-zA-Z0-9_]*$"
-  if( !grepl(regex_pattern_1, project_name) ) stop("The 'project_name' parameter must contain only letters, numbers, and underscores.")
-  if( !grepl(regex_pattern_2, schema_name) ) stop("The 'schema_name' parameter must contain only letters, numbers, and underscores.  It may optionally be enclosed in square brackets.")
-  if( !grepl(regex_pattern_2, procedure_name) ) stop("The 'procedure_name' parameter must contain only letters, numbers, and underscores.  It may optionally be enclosed in square brackets.")
-  if( !grepl(regex_pattern_3, variable_name_project) ) stop("The 'variable_name_project' parameter must contain only letters, numbers, and underscores.  It may optionally have a leading ampersand.")
-  if( !grepl(regex_pattern_1, field_name_token) ) stop("The 'field_name_token' parameter must contain only letters, numbers, and underscores.")
+  
+  if( !grepl(regex_pattern_1, project_name) ) 
+    stop("The 'project_name' parameter must contain only letters, numbers, and underscores.")
+  if( !grepl(regex_pattern_2, schema_name) ) 
+    stop("The 'schema_name' parameter must contain only letters, numbers, and underscores.  It may optionally be enclosed in square brackets.")
+  if( !grepl(regex_pattern_2, procedure_name) ) 
+    stop("The 'procedure_name' parameter must contain only letters, numbers, and underscores.  It may optionally be enclosed in square brackets.")
+  if( !grepl(regex_pattern_3, variable_name_project) ) 
+    stop("The 'variable_name_project' parameter must contain only letters, numbers, and underscores.  It may optionally have a leading ampersand.")
+  if( !grepl(regex_pattern_1, field_name_token) ) 
+    stop("The 'field_name_token' parameter must contain only letters, numbers, and underscores.")
   
   sql <- base::sprintf("EXEC %s.%s %s = '%s'", schema_name, procedure_name, variable_name_project, project_name)
 
   if( base::missing(channel) | base::is.null(channel) ) {
-    if( base::missing(dsn) | base::is.null(dsn) ) stop("The 'dsn' parameter can be missing only if a 'channel' has been passed to 'retrieve_token_mssql'.")
+    if( base::missing(dsn) | base::is.null(dsn) ) 
+      stop("The 'dsn' parameter can be missing only if a 'channel' has been passed to 'retrieve_token_mssql'.")
 
     channel <- RODBC::odbcConnect(dsn=dsn)
     close_channel_on_exit <- TRUE
