7d09accc8c56ed9bfaef19742dad5a0a53218689
Will Beasley <wibeasley@hotmail.com>
2015-05-10 18:17:03 -0500

    split column sanitize

--- tests/testthat/test-column-sanitize.R
+++ tests/testthat/test-column-sanitize.R
@@ -4,11 +4,35 @@ library(testthat)
 context("Sanitize")
 ###########
 
+
+test_that("dry_run", {
+  dirty <- data.frame(id=seq_along(letters), names=letters, stringsAsFactors=FALSE) #These aren't really dirty.  And should have no conversion problems
+  
+  expected <- structure(list(id = as.character(1:26), names = letters), 
+                        .Names = c("id", "names"), row.names = c(NA, -26L), class = "data.frame")
+ 
+
+  observed <- REDCapR::redcap_column_sanitize(dirty)
+  expect_equal(observed, expected, label="The dry-runsanitized values should be correct.")
+})
+
 test_that("sanitize_last_names", {
   dirty <- data.frame(id=1:3, names=c("Ekstr\xf8m", "J\xf6reskog", "bi\xdfchen Z\xfcrcher"))
-  expected <- structure(list(id = c("1", "2", "3"), names = c("Ekstr?m", "Joreskog", 
+  
+  expected_linux <- structure(list(id = c("1", "2", "3"), names = c("Ekstr?m", "Joreskog", 
     "bisschen Zurcher")), .Names = c("id", "names"), row.names = c(NA, 
     -3L), class = "data.frame")
+  
+  expected_windows <- structure(list(id = c("1", "2", "3"), names = c("Ekstrom", "Joreskog", 
+    "bi?chen Zurcher")), .Names = c("id", "names"), row.names = c(NA, 
+    -3L), class = "data.frame")
+  
+  #The different OSes can have subtly different conversions, b/c they're based on different underlying conversion libraries.
+  if( Sys.info()["sysname"]=="Windows" ) {
+    expected <- expected_windows
+  } else {
+    expected <- expected_linux
+  }
 
   observed <- REDCapR::redcap_column_sanitize(dirty)
   expect_equal(observed, expected, label="The sanitized values should be correct.")
--- tests/testthat/test-file_oneshot.R
+++ tests/testthat/test-file_oneshot.R
@@ -165,7 +165,6 @@ test_that("RelativePath", {
   expect_more_than(info_actual$atime, expected=start_time, label="The downloaded file's last access time should not precede this function's start time.")
 })
 
-
 test_that("Full Directory Specific", {
   testthat::skip_on_cran()
   start_clean_result <- REDCapR:::clean_start_simple(batch=FALSE)
@@ -174,58 +173,6 @@ test_that("Full Directory Specific", {
   expected_outcome_message <- "5 records and 24 columns were read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\."
   expect_message(
     returned_object <- redcap_read_oneshot(redcap_uri=project$redcap_uri, token=project$token, raw_or_label="raw"),
-    regexp = expected_outcome_message
-  )
-  
-  start_time <- Sys.time() - 1 #Knock off a second in case there are small time imprecisions
-  path_of_expected <- base::file.path(devtools::inst(name="REDCapR"), "test_data/mugshot_3.jpg")
-  directory <- getwd()#  base::tempfile()
-  info_expected <- file.info(path_of_expected)
-  record <- 3
-  field <- "mugshot"
-  
-  expected_outcome_message <- 'image/jpeg; name="mugshot_3\\.jpg" successfully downloaded in \\d+(\\.\\d+\\W|\\W)seconds\\, and saved as .+\\.jpg'
-  
-  tryCatch({
-    expect_message(
-      returned_object <- redcap_download_file_oneshot(directory=directory, record=record, field=field, redcap_uri=start_clean_result$redcap_project$redcap_uri, token=start_clean_result$redcap_project$token),
-      # returned_object <- redcap_download_file_oneshot(record=record, field=field, redcap_uri=start_clean_result$redcap_project$redcap_uri, token=start_clean_result$redcap_project$token),
-      regexp = expected_outcome_message
-    )
-    Sys.sleep(delay_after_download_file)
-    
-    info_actual <- file.info(returned_object$file_name)
-    expect_true(file.exists(returned_object$file_name), "The downloaded file should exist.")
-  }, finally = base::unlink(returned_object$file_path)
-  )
-  
-  #Test the values of the returned object.
-  expect_true(returned_object$success)
-  expect_equal(returned_object$status_code, expected=200L)
-  expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
-  expect_equal(returned_object$records_affected_count, 1L)
-  expect_equal(returned_object$affected_ids, 3L)
-  expect_true(returned_object$elapsed_seconds>0, "The `elapsed_seconds` should be a positive number.")  
-  expect_equivalent(returned_object$raw_text, expected="") # dput(returned_object$raw_text)
-  expect_equal(returned_object$file_name, returned_object$file_name, label="The name of the downloaded file should be correct.")
-  
-  #Test the values of the file.  
-  expect_equal(info_actual$size, expected=info_expected$size, label="The size of the downloaded file should match.")
-  expect_false(info_actual$isdir, "The downloaded file should not be a directory.")
-  # expect_equal(as.character(info_actual$mode), expected=as.character(info_expected$mode), label="The mode/permissions of the downloaded file should match.")
-  expect_more_than(info_actual$mtime, expected=start_time, label="The downloaded file's modification time should not precede this function's start time.")
-  expect_more_than(info_actual$ctime, expected=start_time, label="The downloaded file's last change time should not precede this function's start time.")
-  expect_more_than(info_actual$atime, expected=start_time, label="The downloaded file's last access time should not precede this function's start time.")
-})
-
-test_that("Bad URL", {
-  testthat::skip_on_cran()
-  start_clean_result <- REDCapR:::clean_start_simple(batch=FALSE)
-  project <- start_clean_result$redcap_project
-  
-  expected_outcome_message <- "5 records and 24 columns were read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\."
-  expect_message(
-    returned_object <- redcap_read_oneshot(redcap_uri="Bad URL", token=project$token, raw_or_label="raw"),
     regexp = expected_outcome_message
   )
   
