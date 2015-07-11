089368590ba1715b15ae5e5bbe51c889d8fbd788
Will Beasley <wibeasley@hotmail.com>
2015-07-11 00:10:47 -0500

    correct pid in comments

--- tests/testthat/test-read_russian.R
+++ tests/testthat/test-read_russian.R
@@ -4,40 +4,40 @@ library(testthat)
 context("Russian Unencoded")
 ###########
 uri <- "https://bbmc.ouhsc.edu/redcap/api/"
-token <- "D72C6485B52FE9F75D27B696977FBA43" #For `UnitTestPhiFree` account on pid=153.
+token <- "D72C6485B52FE9F75D27B696977FBA43" #For `UnitTestPhiFree` account on pid=268.
 
-test_that("Russian Recruit", {  
+test_that("Russian Recruit", {
   testthat::skip_on_cran()
   expect_message(
-    returned_object <- redcap_read_oneshot(redcap_uri=uri, token=token, verbose=T)    
+    returned_object <- redcap_read_oneshot(redcap_uri=uri, token=token, verbose=T)
   )
-  
+
   d <- returned_object$data
   # d$recruitment_other
   # message(d$recruitment_other)
-  
+
   expected_single <- "от сотрудницы"
   expected_multiple <- c("от сотрудницы", "мама и сестра", "подруга по общежитию")
   # expected_multiple <- c("a", "b", "c")
   # Encoding(expected_multiple) <- "latin1"
   # Sys.getlocale()
   # Experiment w/ Joe Cheng's answer at http://stackoverflow.com/questions/5031630/how-to-source-r-file-saved-using-utf-8-encoding
-  
+
 #   expect_equal(d$recruitment_other[1], expected_single)
 #   expect_equal(d$recruitment_other, expected_multiple)
 })
 
-# test_that("Russian Encoded", {  
+# test_that("Russian Encoded", {
 #   testthat::skip_on_cran()
 #   expect_message(
-#     returned_object <- redcap_read_oneshot(redcap_uri=uri, token=token, verbose=T)    
+#     returned_object <- redcap_read_oneshot(redcap_uri=uri, token=token, verbose=T)
 #   )
-#   
+#
 #   d <- returned_object$data
 #   d$recruitment_other
-#   
-#   expected <- c("<U+043E><U+0442> <U+0441><U+043E><U+0442><U+0440><U+0443><U+0434><U+043D><U+0438><U+0446><U+044B>", 
-#                 "<U+043C><U+0430><U+043C><U+0430> <U+0438> <U+0441><U+0435><U+0441><U+0442><U+0440><U+0430>", 
+#
+#   expected <- c("<U+043E><U+0442> <U+0441><U+043E><U+0442><U+0440><U+0443><U+0434><U+043D><U+0438><U+0446><U+044B>",
+#                 "<U+043C><U+0430><U+043C><U+0430> <U+0438> <U+0441><U+0435><U+0441><U+0442><U+0440><U+0430>",
 #                 "<U+043F><U+043E><U+0434><U+0440><U+0443><U+0433><U+0430> <U+043F><U+043E> <U+043E><U+0431><U+0449><U+0435><U+0436><U+0438><U+0442><U+0438><U+044E>")
 #   iconv("н", "UTF-8")
 #   iconv("s", "UTF-8")
