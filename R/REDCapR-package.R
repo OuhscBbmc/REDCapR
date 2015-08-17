04fb2c6ca391c5c16ed323a55a9876280dae3313
Will Beasley <wibeasley@hotmail.com>
2015-08-17 10:13:47 -0500

    Redirects to release version on GitHub

--- R/REDCapR-package.R
+++ R/REDCapR-package.R
@@ -9,4 +9,13 @@
 #' \emph{OUHSC CCAN Independent Evaluation of the State of Oklahoma Competitive Maternal, Infant, and Early Childhood Home Visiting 
 #' (\href{http://mchb.hrsa.gov/programs/homevisiting/}{MIECHV}) Project.}, which
 #' evaluates MIECHV expansion and enhancement of Evidence-based Home Visitation programs in four Oklahoma counties.
+#' 
+#' @note The release version is available through \href{https://cran.r-project.org/package=REDCapR}{CRAN} by   
+#' running \code{install.packages('REDCapR')}.  
+#' The most recent development version is available through \href{https://github.com/OuhscBbmc/REDCapR}{GitHub} by   
+#' running 
+#' \code{devtools::install_github} \code{(repo = 'OuhscBbmc/REDCapR')}   
+#' (make sure \href{https://cran.r-project.org/package=devtools}{devtools} is already installed).
+#' If you're having trouble with the package, please install the development version.  If this doesn't solve
+#' your problem, please create a \href{https://github.com/OuhscBbmc/REDCapR/issues}{new issue}, or email Will.
 NULL
