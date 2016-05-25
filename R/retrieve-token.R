d291e2eeb1e0987427d380b8cfa04527cf79bec9
Will Beasley <wibeasley@hotmail.com>
2016-05-24 21:15:16 -0700

    Add `retrieve_token_local()`.
    Still needs tests and working example.

--- R/retrieve-token.R
+++ R/retrieve-token.R
@@ -27,24 +27,20 @@
 #'
 #' @note
 #' We use Microsoft SQL Server, because that fits our University's infrastructure the easiest.  But this approach theoretically can work
-#' with any LDAP-enabled database server.  Please contact us if your institution is using something other than SQL Server, and would like help adapting this approach to
-#' your infrastructure.
+#' with any LDAP-enabled database server.  Please contact us if your institution is using something other than SQL Server, and 
+#' would like help adapting this approach to your infrastructure.
 #' @author Will Beasley
 #'
 #' @examples
-#' \dontrun{
 #' library(REDCapR) #Load the package into the current R session.
-#'
-#' ##
-#' ## Rely on `retrieve_token()` to create & destory the channel.
-#' ##
+#' \dontrun{
+#' # ---- SQL Server Example ----------------------------
+#' # Rely on `retrieve_token()` to create & destory the channel.
 #' dsn <- "TokenSecurity"
 #' project <- "DiabetesSurveyProject"
 #' token <- retrieve_token(dsn=dsn, project_name=project)
 #'
-#' ##
-#' ## Create & close the channel yourself, to optimize repeated calls.
-#' ##
+#' # Create & close the channel yourself, to optimize repeated calls.
 #' dsn <- "TokenSecurity"
 #' project1 <- "DiabetesSurveyProject1"
 #' project2 <- "DiabetesSurveyProject2"
@@ -56,7 +52,13 @@
 #' token3 <- retrieve_token(dsn=dsn, project_name=project3)
 #' RODBC::odbcClose(channel)
 #' }
-#'
+#' 
+#' \dontrun{
+#' # ---- Local File Example ----------------------------
+#' path <- system.file("misc/example.credentials", package="REDCapR")
+#' project_id  <- 153
+#' retrieve_token_local(path, project_id)
+#' }
 
 retrieve_token_mssql <- function(
   dsn,
@@ -66,7 +68,7 @@ retrieve_token_mssql <- function(
   procedure_name           = "[prcToken]",
   variable_name_project    = "@RedcapProjectName",
   field_name_token         = "Token"
-  ) {
+) {
 
   if( !requireNamespace("RODBC", quietly=TRUE) ) 
     stop("The function REDCapR::retrieve_token_mssql() cannot run if the `RODBC` package is not installed.  Please install it and try again.")
@@ -107,4 +109,67 @@ retrieve_token_mssql <- function(
   )
 
   return( token )
+}
+
+
+retrieve_token_local <- function(
+  path_credential,
+  project_id,
+  check_url                = TRUE,
+  check_username           = FALSE,
+  check_token_pattern      = TRUE
+) {
+
+  # Check that the file exists and read it into a data frame.
+  if( !file.exists(path_credential) ) stop("The credential file was not found.")
+  ds_credentials <- readr::read_csv(path_credential, comment = "#")
+  
+  # Select only the records with a matching project id.
+  ds_credential <- ds_credentials[ds_credentials$project_id==project_id, ]
+  
+  # Check that one and only one record matches the project id.
+  if( nrow(ds_credential)==0L ) {
+    stop("The project_id was not found in the csv credential file.")
+  } else if( nrow(ds_credential) > 1 ) {
+    stop("More than one matching project_id was found in the csv credential file.  There should be only one.")
+  } else {
+    credential <- list(
+      redcap_uri   = ds_credential$redcap_uri[1],
+      username     = ds_credential$username[1],
+      project_id   = ds_credential$project_id[1],
+      token        = ds_credential$token[1],
+      comment      = ds_credential$comment[1]
+    )
+  }
+  
+  # Progress through the optional checks
+  if( check_url & !grepl("https://", credential$redcap_uri, perl=TRUE) ) {
+    error_message_username <- paste(
+      "The REDCap URL does not reference an https address.  First check",
+      "that the URL is correct, and then consider using SSL to encrypt",
+      "the REDCap webserver.  Set the `check_url` parameter to FALSE",
+      "if you're sure you have the correct file & file contents."
+    )
+    stop(error_message_username)
+  } else if( check_username & (Sys.info()["user"]!=credential$username) ) {
+    error_message_username <- paste(
+      "The username (according to R's `Sys.info()['user']` doesn't match the",
+      "username in the credentials file.  This is a friendly check, and",
+      "NOT a security measure.  Set the `check_username` parameter to FALSE",
+      "if you're sure you have the correct file & file contents.",
+      "Otherwise, you may be pointing to the wrong credentials file."
+    )
+    stop(error_message_username)
+  } else if( check_token_pattern & !grepl("[A-Z0-9]{32}", credential$token, perl=TRUE) ) {
+    error_message_token <- paste(
+      "A REDCap token should be a string of 32 digits and uppercase",
+      "characters.  The retrieved value was not.",
+      "Set the `check_token_pattern` parameter to FALSE",
+      "if you're sure you have the correct file & file contents."
+    )
+    stop(error_message_token)
+  }
+  
+  # Return to caller.
+  return( credential )
 }
