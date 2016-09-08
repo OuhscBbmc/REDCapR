f53d0936487d336c9393254007fd19730c090215
Will Beasley <wibeasley@hotmail.com>
2016-09-07 23:10:30 -0500

    more canonical https

--- R/redcap-read-oneshot.R
+++ R/redcap-read-oneshot.R
@@ -1,9 +1,9 @@
 #' @name redcap_read_oneshot
 #' @export redcap_read_oneshot
 #' @title Read/Export records from a REDCap project.
-#'  
+#'
 #' @description This function uses REDCap's API to select and return data.
-#' 
+#'
 #' @param redcap_uri The URI (uniform resource identifier) of the REDCap project.  Required.
 #' @param token The user-specific string that serves as the password for a project.  Required.
 #' @param records An array, where each element corresponds to the ID of a desired record.  Optional.
@@ -29,19 +29,19 @@
 #'  \item \code{elapsed_seconds}: The duration of the function.
 #'  \item \code{raw_text}: If an operation is NOT successful, the text returned by REDCap.  If an operation is successful, the `raw_text` is returned as an empty string to save RAM.
 #' }
-#' @details 
+#' @details
 #' The full list of configuration options accepted by the \code{httr} package is viewable by executing \code{httr::httr_options()}.  The \code{httr}
-#' package and documentation is available at \url{http://cran.r-project.org/package=httr}.
-#' 
+#' package and documentation is available at \url{https://cran.r-project.org/package=httr}.
+#'
 #' If you do not pass in this export_data_access_groups value, it will default to \code{FALSE}. The following is from the API help page for version 5.2.3: This flag is only viable if the user whose token is being used to make the API request is *not* in a data access group. If the user is in a group, then this flag will revert to its default value.
 #' @author Will Beasley
-#' @references The official documentation can be found on the `API Help Page` and `API Examples' pages 
+#' @references The official documentation can be found on the `API Help Page` and `API Examples' pages
 #' on the REDCap wiki (ie, \url{https://community.projectredcap.org/articles/456/api-documentation.html} and
-#' \url{https://community.projectredcap.org/articles/462/api-examples.html}). If you do not have an account 
+#' \url{https://community.projectredcap.org/articles/462/api-examples.html}). If you do not have an account
 #' for the wiki, please ask your campus REDCap administrator to send you the static material.
-#' 
+#'
 #' The official \href{http://curl.haxx.se}{cURL site} discusses the process of using SSL to verify the server being connected to.
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' library(REDCapR) #Load the package into the current R session.
@@ -49,74 +49,74 @@
 #' token    <- "9A81268476645C4E5F03428B8AC3AA7B"
 #' #Return all records and all variables.
 #' ds_all_rows_all_fields <- redcap_read_oneshot(redcap_uri=uri, token=token)$data
-#' 
+#'
 #' #Return only records with IDs of 1 and 3
 #' desired_records_v1 <- c(1, 3)
 #' ds_some_rows_v1 <- redcap_read_oneshot(
-#'    redcap_uri = uri, 
-#'    token      = token, 
+#'    redcap_uri = uri,
+#'    token      = token,
 #'    records    = desired_records_v1
 #' )$data
-#' 
+#'
 #' #Return only the fields record_id, name_first, and age
 #' desired_fields_v1 <- c("record_id", "name_first", "age")
 #' ds_some_fields_v1 <- redcap_read_oneshot(
-#'    redcap_uri = uri, 
-#'    token      = token, 
+#'    redcap_uri = uri,
+#'    token      = token,
 #'    fields     = desired_fields_v1
 #' )$data
-#' 
-#' 
+#'
+#'
 #' #Use the SSL cert file that come with the openssl package.
 #' cert_location <- system.file("cacert.pem", package="openssl")
 #' if( file.exists(cert_location) ) {
 #'   config_options         <- list(cainfo=cert_location)
 #'   ds_different_cert_file <- redcap_read_oneshot(
 #'     redcap_uri     = uri,
-#'     token          = token, 
+#'     token          = token,
 #'     config_options = config_options
 #'   )$data
 #' }
-#' 
+#'
 #' #Force the connection to use SSL=3 (which is not preferred, and possibly insecure).
 #' config_options <- list(sslversion=3)
 #' ds_ssl_3 <- redcap_read_oneshot(
 #'   redcap_uri     = uri,
-#'   token          = token, 
+#'   token          = token,
 #'   config_options = config_options
 #' )$data
-#' 
+#'
 #' config_options <- list(ssl.verifypeer=FALSE)
 #' ds_no_ssl <- redcap_read_oneshot(
 #'    redcap_uri     = uri,
-#'    token          = token, 
+#'    token          = token,
 #'    config_options = config_options
 #' )$data
 #' }
 
-redcap_read_oneshot <- function( 
-  redcap_uri, token, records=NULL, records_collapsed="", 
-  fields=NULL, fields_collapsed="", 
+redcap_read_oneshot <- function(
+  redcap_uri, token, records=NULL, records_collapsed="",
+  fields=NULL, fields_collapsed="",
   events=NULL, events_collapsed="",
   export_data_access_groups=FALSE,
   filter_logic="",
-  raw_or_label='raw', verbose=TRUE, config_options=NULL 
+  raw_or_label='raw', verbose=TRUE, config_options=NULL
 ) {
   #TODO: NULL verbose parameter pulls from getOption("verbose")
   #TODO: warns if any requested fields aren't entirely lowercase.
   #TODO: validate export_data_access_groups
   #TODO: validate raw_or_label
-  
+
   start_time <- Sys.time()
-  
+
   if( missing(redcap_uri) )
     stop("The required parameter `redcap_uri` was missing from the call to `redcap_read_oneshot()`.")
-  
+
   if( missing(token) )
     stop("The required parameter `token` was missing from the call to `redcap_read_oneshot()`.")
-  
+
   token <- sub("\\n", "", token)
-  
+
   if( all(nchar(records_collapsed)==0) )
     records_collapsed <- ifelse(is.null(records), "", paste0(records, collapse=",")) #This is an empty string if `records` is NULL.
   if( (length(fields_collapsed)==0L) | is.null(fields_collapsed) | all(nchar(fields_collapsed)==0L) )
@@ -125,7 +125,7 @@ redcap_read_oneshot <- function(
     events_collapsed <- ifelse(is.null(events), "", paste0(events, collapse=",")) #This is an empty string if `events` is NULL.
   if( all(nchar(filter_logic)==0) )
     filter_logic <- ifelse(is.null(filter_logic), "", filter_logic) #This is an empty string if `filter_logic` is NULL.
-  
+
   export_data_access_groups_string <- ifelse(export_data_access_groups, "true", "false")
 
   post_body <- list(
@@ -149,16 +149,16 @@ redcap_read_oneshot <- function(
 
   status_code <- result$status
   success <- (status_code==200L)
-  
-  raw_text <- httr::content(result, "text")  
+
+  raw_text <- httr::content(result, "text")
   elapsed_seconds <- as.numeric(difftime(Sys.time(), start_time, units="secs"))
-  
+
   # raw_text <- "The hostname (redcap-db.hsc.net.ou.edu) / username (redcapsql) / password (XXXXXX) combination could not connect to the MySQL server. \r\n\t\tPlease check their values."
   regex_cannot_connect <- "^The hostname \\((.+)\\) / username \\((.+)\\) / password \\((.+)\\) combination could not connect.+"
-  
-  if( any(grepl(regex_cannot_connect, raw_text)) ) 
+
+  if( any(grepl(regex_cannot_connect, raw_text)) )
     success <- FALSE
-  
+
   if( success ) {
     try (
       {
@@ -167,15 +167,15 @@ redcap_read_oneshot <- function(
       }, #Convert the raw text to a dataset.
       silent = TRUE #Don't print the warning in the try block.  Print it below, where it's under the control of the caller.
     )
-    
+
     #TODO #80: catch variant of ' The.hostname..redcap.db.hsc.net.ou.edu....username..redcapsql....password..XXXXXX..combination.could.not.connect.to.the.MySQL.server. \t\tPlease check their values.'
-    
+
     if( exists("ds") & inherits(ds, "data.frame") ) {
       outcome_message <- paste0(
-        format(nrow(ds), big.mark=",", scientific=FALSE, trim=TRUE), 
-        " records and ",  
-        format(length(ds), big.mark=",", scientific=FALSE, trim=TRUE), 
-        " columns were read from REDCap in ", 
+        format(nrow(ds), big.mark=",", scientific=FALSE, trim=TRUE),
+        " records and ",
+        format(length(ds), big.mark=",", scientific=FALSE, trim=TRUE),
+        " columns were read from REDCap in ",
         round(elapsed_seconds, 1), " seconds.  The http status code was ",
         status_code, "."
       )
@@ -185,23 +185,23 @@ redcap_read_oneshot <- function(
       #   ds,
       #   is.character,
       #   function(x) dplyr::coalesce(x, "") #Replace NAs with blanks
-      # ) 
-      # 
+      # )
+      #
       # ds <- dplyr::mutate_if(
       #   ds,
       #   is.character,
       #   function( x ) gsub("\r\n", "\n", x, perl=TRUE)
-      # ) 
+      # )
       # ds <- dplyr::mutate_if(
       #   ds,
       #   function( x) inherits(x, "Date"),
       #   as.character
-      # ) 
-      # 
+      # )
+      #
       # ds <- base::as.data.frame(ds)
-    
+
       #If an operation is successful, the `raw_text` is no longer returned to save RAM.  The content is not really necessary with httr's status message exposed.
-      raw_text <- "" 
+      raw_text <- ""
     } else {
       success          <- FALSE #Override the 'success' determination from the http status code.
       ds               <- data.frame() #Return an empty data.frame
@@ -212,16 +212,16 @@ redcap_read_oneshot <- function(
     ds                 <- data.frame() #Return an empty data.frame
     outcome_message    <- paste0("The REDCapR read/export operation was not successful.  The error message was:\n",  raw_text)
   }
-    
-  if( verbose ) 
+
+  if( verbose )
     message(outcome_message)
-  
+
   return( list(
-    data               = ds, 
+    data               = ds,
     success            = success,
     status_code        = status_code,
     outcome_message    = outcome_message,
-    records_collapsed  = records_collapsed, 
+    records_collapsed  = records_collapsed,
     fields_collapsed   = fields_collapsed,
     filter_logic       = filter_logic,
     events_collapsed   = events_collapsed,
