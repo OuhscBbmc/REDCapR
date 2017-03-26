37f81399e307f32b721b95f2b1872e40c6f5ccd2
Will Beasley <wibeasley@hotmail.com>
2017-03-26 15:48:26 -0500

    stop on error if doesn't validate

--- R/validate.R
+++ R/validate.R
@@ -4,9 +4,9 @@
 #' @usage
 #' validate_for_write( d )
 #'
-#' validate_no_logical( data_types )
+#' validate_no_logical( data_types, stop_on_error )
 #'
-#' validate_field_names( field_names )
+#' validate_field_names( field_names, stop_on_error )
 #'
 #' @title Inspect a [base::data.frame()] to anticipate problems before writing to a REDCap project.
 #'
@@ -15,6 +15,7 @@
 #' @param d The [base::data.frame()] containing the dataset used to update the REDCap project.
 #' @param data_types The data types of the [base::data.frame()] corresponding to the REDCap project.
 #' @param field_names The names of the fields/variables in the REDCap project.
+#' @param stop_on_error If `TRUE`, an error is thrown for violations.  Otherwise, a dataset summarizing the problems is returned.
 #'
 #' @return A [tibble::tibble()], where each potential violation is a row.  The two columns are:
 #' * `field_name`: The name of the [base::data.frame()] that might cause problems during the upload.
@@ -41,7 +42,7 @@
 #' validate_for_write(d = d)
 
 
-validate_no_logical <- function( data_types ) {
+validate_no_logical <- function( data_types, stop_on_error=FALSE ) {
   # indices <- which(sapply(d, class)=="logical")
   indices <- which(data_types=="logical")
 
@@ -52,6 +53,8 @@ validate_no_logical <- function( data_types ) {
       concern            = character(0),
       suggestion         = character(0)
     )
+  } else if( stop_on_error) {
+    stop(length(indices), " field(s) were logical/boolean.  The REDCap API does not automatically convert boolean values to 0/1 values.  Convert the variable with the `as.integer()` function.")
   } else {
     tibble::tibble(
       field_name         = names(data_types)[indices],
@@ -61,7 +64,7 @@ validate_no_logical <- function( data_types ) {
     )
   }
 }
-validate_field_names <- function( field_names ) {
+validate_field_names <- function( field_names, stop_on_error=FALSE ) {
   pattern <- "^[0-9a-z_]+$"
 
   indices <- which(!grepl(pattern, x=field_names, perl=TRUE))
@@ -72,6 +75,8 @@ validate_field_names <- function( field_names ) {
       concern            = character(0),
       suggestion         = character(0)
     )
+  } else if( stop_on_error) {
+    stop(length(indices), " field name(s) violated the naming rules.  Only digits, lowercase letters, and underscores are allowed.")
   } else {
     tibble::tibble(
       field_name         = field_names[indices],
