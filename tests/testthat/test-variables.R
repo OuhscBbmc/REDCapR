8e21056990238d65cefec0c63c7575cb8a28ec14
Will Beasley <wibeasley@hotmail.com>
2017-08-21 21:46:02 -0500

    test bad URI for variable call
    See #173

--- tests/testthat/test-variables.R
+++ tests/testthat/test-variables.R
@@ -41,3 +41,25 @@ test_that("All Records -Default", {
   expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
   expect_true(returned_object$success)
 })
+
+
+test_that("All Records -bad uri", {
+  testthat::skip_on_cran()
+  bad_uri <- "google.com"
+  expected_data_frame <- structure(list(), .Names = character(0), row.names = integer(0), class = "data.frame")
+
+  expected_outcome_message <- "The REDCapR variable retrieval was not successful\\..+?Error 405 \\(Method Not Allowed\\).+"
+   # expected_outcome_message <- "(?s)The REDCapR variable retrieval was not successful\\..+?.+"
+
+  expect_message(
+    returned_object <- redcap_variables(redcap_uri=bad_uri, token=credential$token),
+    regexp = expected_outcome_message
+  )
+  expected_outcome_message <- paste0("(?s)", expected_outcome_message)
+
+  expect_equal(returned_object$data, expected=expected_data_frame, label="The returned data.frame should be correct") # dput(returned_object$data)
+  expect_equal(returned_object$status_code, expected=405L)
+  # expect_equivalent(returned_object$raw_text, expected="") # dput(returned_object$raw_text)
+  expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
+  expect_false(returned_object$success)
+})
