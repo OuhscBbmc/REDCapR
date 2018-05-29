ce2de5569124ff25fa96158bfb7723671ad385d6
Will Beasley <wibeasley@hotmail.com>
2018-05-29 11:56:17 -0500

    rename utilities test file
    in preparation for #209

--- tests/testthat/test-utilities-replace_nas_with_explicit.R
+++ tests/testthat/test-utilities-replace_nas_with_explicit.R
@@ -1,17 +1,13 @@
 library(testthat)
-context("Replace NAs")
+context("collapse_vector")
 
-test_that("replace_nas_character_standard", {
-  a <- letters
-  missing_indices <- c(3, 6, 8, 25)
-  a[missing_indices] <- NA_character_
+test_that("both NULL", {
+  elements  <- NULL
+  collapsed <- NULL
+  expected  <- ""
 
-  expected <- c("a", "b", "Unknown", "d", "e", "Unknown", "g", "Unknown", "i", "j", "k", "l", "m",
-                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "Unknown", "z")
-
-  a <- REDCapR:::replace_nas_with_explicit(a)
-  expect_equal(a, expected, label="The correct letters should have been replaced.")
-  expect_equal(class(a), "character", "The returned array should remain a character.")
+  observed <- REDCapR:::collapse_vector(elements, collapsed)
+  expect_equal(observed, expected)
 })
 
 test_that("replace_nas_factor_standard", {
