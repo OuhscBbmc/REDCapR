2299a56c19d15253a8fd65b442473f74808a5b37
Will Beasley <wibeasley@hotmail.com>
2018-09-16 15:47:00 -0500

    add `redcap_users_export()`
    closes #163

--- utility/refresh.R
+++ utility/refresh.R
@@ -14,11 +14,13 @@ devtools::run_examples(); #dev.off() #This overwrites the NAMESPACE file too
 # devtools::run_examples(, "redcap_read.Rd")
 test_results_checked <- devtools::test()
 test_results_checked <- devtools::test(filter = "read-oneshot-eav")
-test_results_checked <- devtools::test(filter = "next-free.*$")
+test_results_checked <- devtools::test(filter = "users.*$")
 # testthat::test_dir("./tests/")
 test_results_not_checked <- testthat::test_dir("./tests/manual/")
 
 # devtools::check(force_suggests = FALSE)
+devtools::check(cran=T)
+# devtools::check_rhub(email="wibeasley@hotmail.com")
 # devtools::build_win(version="R-devel") #CRAN submission policies encourage the development version
 # devtools::revdep_check(pkg="REDCapR", recursive=TRUE)
 # devtools::release(check=FALSE) #Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
