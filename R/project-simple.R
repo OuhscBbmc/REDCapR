651780085fa9824d18df2f425d904e98f8e9d257
Will Beasley <wibeasley@hotmail.com>
2019-11-14 00:25:00 -0600

    lint redcap-next-free-record-name & project-simple
    ref #238

--- R/project-simple.R
+++ R/project-simple.R
@@ -1,21 +1,37 @@
 # These functions are not exported.
 
-populate_project_simple <- function( batch = FALSE ) {
-  checkmate::assert_logical(batch, any.missing=F, len=1)
+#' @examples
+#' \dontrun{
+#' populate_project_simple()
+#' populate_project_simple(batch = TRUE)
+#' clear_project_simple()
+#' clean_start_simple()
+#' clean_start_simple(batch = TRUE)
+#' }
+populate_project_simple <- function(batch = FALSE) {
+  checkmate::assert_logical(batch, any.missing = F, len = 1)
 
-  if( !requireNamespace("testthat") )
-    stop("The function REDCapR:::populate_project_simple() cannot run if the `testthat` package is not installed.  Please install it and try again.") # nocov
+  if (!requireNamespace("testthat")) {
+    # nocov start
+    stop(
+      "The function REDCapR:::populate_project_simple() cannot run if the ",
+      "`testthat` package is not installed.  Please install it and try again."
+    )
+    # nocov stop
+  }
 
-  #Declare the server & user information
-  # uri <- "https://www.redcapplugins.org/api/"
+  # Declare the server & user information
   uri <- "https://bbmc.ouhsc.edu/redcap/api/"
 
   # token <- "D96029BFCE8FFE76737BFC33C2BCC72E" #For `UnitTestPhiFree` account and the simple project (pid 27) on Vandy's test server.
   # token <- "9A81268476645C4E5F03428B8AC3AA7B" #For `UnitTestPhiFree` account and the simple project (pid 153)
   token <- "D70F9ACD1EDD6F151C6EA78683944E98" #For `UnitTestPhiFree` account and the simple project (pid 213)
 
-  project <- REDCapR::redcap_project$new(redcap_uri=uri, token=token)
-  path_in_simple <- system.file("test-data/project-simple/simple-data.csv", package="REDCapR")
+  project <- REDCapR::redcap_project$new(redcap_uri = uri, token = token)
+  path_in_simple <- system.file(
+    "test-data/project-simple/simple-data.csv",
+    package = "REDCapR"
+  )
 
   # Write the file to disk (necessary only when you wanted to change the data).  Don't uncomment; just run manually.
   # returned_object <- redcap_read_oneshot(redcap_uri=uri, token=token, raw_or_label="raw")
@@ -23,55 +39,83 @@ populate_project_simple <- function( batch = FALSE ) {
   # returned_object_metadata <- redcap_metadata_read(redcap_uri=uri, token=token)
   # utils::write.csv(returned_object_metadata$data, file="./inst/test-data/project-simple/simple-metadata.csv", row.names=FALSE)
 
-  #Read in the data in R's memory from a csv file.
-  dsToWrite <- readr::read_csv(path_in_simple)
-  # dsToWrite <- utils::read.csv(file="./inst/test-data/project-simple/simple-data.csv", stringsAsFactors=FALSE)
+  # Read in the data in R's memory from a csv file.
+  ds_to_write <- readr::read_csv(path_in_simple)
+  # ds_to_write <- utils::read.csv(file="./inst/test-data/project-simple/simple-data.csv", stringsAsFactors=FALSE)
 
-  #Remove the calculated variables.
-  dsToWrite$age <- NULL
-  dsToWrite$bmi <- NULL
+  # Remove the calculated variables.
+  ds_to_write$age <- NULL
+  ds_to_write$bmi <- NULL
 
   # Import the data into the REDCap project
   testthat::expect_message(
-    returned_object <- if( batch ) {
-      REDCapR::redcap_write(        ds=dsToWrite, redcap_uri=uri, token=token, verbose=TRUE)
+    returned_object <- if (batch) {
+      REDCapR::redcap_write(
+        ds          = ds_to_write,
+        redcap_uri  = uri,
+        token       = token,
+        verbose     = TRUE
+      )
     } else {
-      REDCapR::redcap_write_oneshot(ds=dsToWrite, redcap_uri=uri, token=token, verbose=TRUE)
+      REDCapR::redcap_write_oneshot(
+        ds          = ds_to_write,
+        redcap_uri  = uri,
+        token       = token,
+        verbose     = TRUE
+      )
     }
   )
-  # For internal inspection: REDCapR::redcap_read_oneshot(redcap_uri=uri, token=token, verbose=TRUE)
 
-  #If uploading the data was successful, then upload the image files.
-  if( returned_object$success) {
-    upload_file_simple(redcap_uri=uri, token=token)
+  # If uploading the data was successful, then upload the image files.
+  if (returned_object$success) {
+    upload_file_simple(redcap_uri = uri, token = token)
   }
 
-  #Print a message and return a boolean value
-  base::message(base::sprintf("populate_project_simple success: %s.", returned_object$success))
-  return( list(is_success=returned_object$success, redcap_project=project) )
+  # Print a message and return a boolean value
+  base::message(base::sprintf(
+    "populate_project_simple success: %s.",
+    returned_object$success
+  ))
+  list(is_success = returned_object$success, redcap_project = project)
 }
-clear_project_simple <- function( verbose = TRUE ) {
-  if( !requireNamespace("testthat") )
-    stop("The function REDCapR:::populate_project_simple() cannot run if the `testthat` package is not installed.  Please install it and try again.") # nocov
-  pathDeleteTestRecord <- "https://bbmc.ouhsc.edu/redcap/plugins/redcapr/delete_redcapr_simple.php"
+clear_project_simple <- function(verbose = TRUE) {
+  if (!requireNamespace("testthat")) {
+    # nocov start
+    stop(
+      "The function REDCapR:::populate_project_simple() cannot run if the ",
+      "`testthat` package is not installed.  Please install it and try again."
+    )
+    # nocov stop
+  }
+  path_delete_test_record <-
+    "https://bbmc.ouhsc.edu/redcap/plugins/redcapr/delete_redcapr_simple.php"
 
-  # httr::url_ok(pathDeleteTestRecord)
+  # Returns a boolean value if successful
+  was_successful <- !httr::http_error(path_delete_test_record)
 
-  #Returns a boolean value if successful
-  (was_successful <- !httr::http_error(pathDeleteTestRecord))#, config=config_options))
+  # Print a message and return a boolean value
+  if (verbose) {
+    base::message(base::sprintf(
+      "clear_project_simple success: %s.",
+      was_successful
+    ))
+  }
 
-  #Print a message and return a boolean value
-  if( verbose )
-    base::message(base::sprintf("clear_project_simple success: %s.", was_successful))
-  return( was_successful )
+  was_successful
 }
 
-clean_start_simple <- function( batch = FALSE, delay_in_seconds = 1 ) {
-  checkmate::assert_logical( batch            , any.missing=F, len=1)
-  checkmate::assert_numeric( delay_in_seconds , any.missing=F, len=1, lower=0)
+clean_start_simple <- function(batch = FALSE, delay_in_seconds = 1) {
+  checkmate::assert_logical(batch           , any.missing=F, len=1)
+  checkmate::assert_numeric(delay_in_seconds, any.missing=F, len=1, lower=0)
 
-  if( !requireNamespace("testthat") )
-    stop("The function REDCapR:::populate_project_simple() cannot run if the `testthat` package is not installed.  Please install it and try again.") # nocov
+  if (!requireNamespace("testthat")) {
+    # nocov start
+    stop(
+      "The function REDCapR:::populate_project_simple() cannot run if the ",
+      "`testthat` package is not installed.  Please install it and try again."
+    )
+    # nocov stop
+  }
   testthat::expect_message(
     clear_result <- clear_project_simple(),
     regexp = "clear_project_simple success: TRUE."
@@ -80,36 +124,39 @@ clean_start_simple <- function( batch = FALSE, delay_in_seconds = 1 ) {
   base::Sys.sleep(delay_in_seconds) #Pause after deleting records.
 
   testthat::expect_message(
-    populate_result <- populate_project_simple(batch=batch),
+    populate_result <- populate_project_simple(batch = batch),
     regexp = "populate_project_simple success: TRUE."
   )
   testthat::expect_true(populate_result$is_success, "Population the the simple project should be successful.")
   base::Sys.sleep(delay_in_seconds) #Pause after writing records.
 
-  return( populate_result )
+  populate_result
 }
 
-upload_file_simple <- function( redcap_uri, token=token ) {
+upload_file_simple <- function(redcap_uri, token = token) {
   checkmate::assert_character(redcap_uri, any.missing=F, len=1, min.chars = 5)
   checkmate::assert_character(token     , any.missing=F, len=1, pattern="^\\w{32}$")
 
   records <- 1:5
-  file_paths <- system.file(paste0("test-data/mugshot-", records, ".jpg"), package="REDCapR")
+  file_paths <- system.file(
+    paste0("test-data/mugshot-", records, ".jpg"),
+    package = "REDCapR"
+  )
 
   field <- "mugshot"
-  event <- "" # only for longitudinal events
+  # event <- "" # only for longitudinal events
 
   token <- sanitize_token(token)
 
-  for( i in seq_along(records) ) {
+  for (i in seq_along(records)) {
     record    <- records[i]
     file_path <- file_paths[i]
-    redcap_upload_file_oneshot(file_name=file_path, record=record, field=field, redcap_uri=redcap_uri, token=token)
+    redcap_upload_file_oneshot(
+      file_name   = file_path,
+      record      = record,
+      field       = field,
+      redcap_uri  = redcap_uri,
+      token       = token
+    )
   }
 }
-
-# populate_project_simple()
-# populate_project_simple(batch=TRUE)
-# clear_project_simple()
-# clean_start_simple()
-# clean_start_simple(batch=TRUE)
--- R/redcap-next-free-record-name.R
+++ R/redcap-next-free-record-name.R
@@ -2,61 +2,94 @@
 #'
 #' @description Determines the next available record ID.
 #'
-#' @param redcap_uri The URI (uniform resource identifier) of the REDCap project.  Required.
-#' @param token The user-specific string that serves as the password for a project.  Required.
-#' @param verbose A boolean value indicating if `message`s should be printed to the R console during the operation.  The verbose output might contain sensitive information (*e.g.* PHI), so turn this off if the output might be visible somewhere public. Optional.
-#' @param config_options  A list of options to pass to `POST` method in the `httr` package.  See the details below.  Optional.
+#' @param redcap_uri The URI (uniform resource identifier) of the REDCap
+#' project.  Required.
+#' @param token The user-specific string that serves as the password for a
+#' project.  Required.
+#' @param verbose A boolean value indicating if `message`s should be printed
+#' to the R console during the operation.  The verbose output might contain
+#' sensitive information (*e.g.* PHI), so turn this off if the output might
+#' be visible somewhere public. Optional.
+#' @param config_options  A list of options to pass to `POST` method in the
+#' `httr` package.  See the details below.  Optional.
 #'
-#' @return a \code{\link[base:character]{base::character}} vector of either length 1 (if successful) or length 0 (if not successful).
+#' @return a \code{\link[base:character]{base::character}} vector of either
+#' length 1 (if successful) or length 0 (if not successful).
 #'
-#' @details If the API call is unsuccessful, a value of `character(0)` will be returned (*i.e.*, an empty vector).
-#' This ensures that a the function will always return an object of class \code{\link[base:character]{base::character}}.
+#' @details If the API call is unsuccessful, a value of `character(0)` will
+#' be returned (*i.e.*, an empty vector).  This ensures that a the function
+#' will always return an object of class
+#' [base:character].
 #'
 #' @note
 #' **Documentation in REDCap 8.4.0**
 #'
-#' To be used by projects with record auto-numbering enabled, this method exports the next potential record ID for a project. It generates the next record name by determining the current maximum numerical record ID and then incrementing it by one.
+#' To be used by projects with record auto-numbering enabled, this method
+#' exports the next potential record ID for a project. It generates the next
+#' record name by determining the current maximum numerical record ID and then
+#' incrementing it by one.
+#'
 #' ```
-#' Note: This method does not create a new record, but merely determines what the next record name would be.
+#' Note: This method does not create a new record, but merely determines what
+#' the next record name would be.
 #'
-#' If using Data Access Groups (DAGs) in the project, this method accounts for the special formatting of the record name for users in DAGs (e.g., DAG-ID); in this case, it only assigns the next value for ID for all numbers inside a DAG. For example, if a DAG has a corresponding DAG number of 223 wherein records 223-1 and 223-2 already exist, then the next record will be 223-3 if the API user belongs to the DAG that has DAG number 223. (The DAG number is auto-assigned by REDCap for each DAG when the DAG is first created.) When generating a new record name in a DAG, the method considers all records in the entire project when determining the maximum record ID, including those that might have been originally created in that DAG but then later reassigned to another DAG.
+#' If using Data Access Groups (DAGs) in the project, this method accounts for
+#' the special formatting of the record name for users in DAGs (e.g., DAG-ID);
+#' in this case, it only assigns the next value for ID for all numbers inside
+#' a DAG. For example, if a DAG has a corresponding DAG number of 223 wherein
+#' records 223-1 and 223-2 already exist, then the next record will be 223-3
+#' if the API user belongs to the DAG that has DAG number 223. (The DAG number
+#' is auto-assigned by REDCap for each DAG when the DAG is first created.)
 #'
-#' Note: This method functions the same even for projects that do not have record auto-numbering enabled.
+#' When generating a new record name in a DAG, the method considers all records
+#' in the entire project when determining the maximum record ID, including
+#' those that might have been originally created in that DAG but then later
+#' reassigned to another DAG.
+#'
+#' Note: This method functions the same even for projects that do not have
+#' record auto-numbering enabled.
 #' ```
 #'
 #' @examples
 #' uri      <- "https://bbmc.ouhsc.edu/redcap/api/"
 #' token    <- "9A81268476645C4E5F03428B8AC3AA7B"
-#' REDCapR::redcap_next_free_record_name(redcap_uri=uri, token=token) # Should return "6"
+
+#' # Returns 6
+#' REDCapR::redcap_next_free_record_name(redcap_uri = uri, token = token)
 
 #' @export
-redcap_next_free_record_name <- function( redcap_uri, token, verbose=TRUE, config_options=NULL ) {
+redcap_next_free_record_name <- function(
+  redcap_uri,
+  token,
+  verbose = TRUE,
+  config_options = NULL
+) {
   value_error       <- character(0)
 
-  checkmate::assert_character(redcap_uri                , any.missing=F, len=1, pattern="^.{1,}$")
-  checkmate::assert_character(token                     , any.missing=F, len=1, pattern="^.{1,}$")
+  checkmate::assert_character(redcap_uri, any.missing=F, len=1, pattern="^.{1,}$")
+  checkmate::assert_character(token     , any.missing=F, len=1, pattern="^.{1,}$")
 
   token   <- sanitize_token(token)
   verbose <- verbose_prepare(verbose)
 
   post_body <- list(
-    token                   = token,
-    content                 = 'generateNextRecordName',
-    format                  = 'csv'
+    token     = token,
+    content   = "generateNextRecordName",
+    format    = "csv"
   )
 
   # This is the important line that communicates with the REDCap server.
   kernel <- kernel_api(redcap_uri, post_body, config_options)
 
-  if( kernel$success ) {
-    try (
-      {
-        value <- kernel$raw_text
-      },
-      silent = TRUE #Don't print the warning in the try block.  Print it below, where it's under the control of the caller.
+  if (kernel$success) {
+    # Don't print the warning in the try block.  Print it below,
+    # where it's under the control of the caller.
+    try(
+      value <- kernel$raw_text
+      , silent = TRUE
     )
 
-    if( exists("value") ) {
+    if (exists("value")) {
       outcome_message <- sprintf(
         "The next free record name in REDCap was successfully determined in %0.1f seconds.  The http status code was %i.  Is is %s.",
         kernel$elapsed_seconds,
@@ -80,8 +113,8 @@ redcap_next_free_record_name <- function( redcap_uri, token, verbose=TRUE, confi
     )
   }  # nocov stop
 
-  if( verbose )
+  if (verbose)
     message(outcome_message)
 
-  return( value )
+  value
 }
