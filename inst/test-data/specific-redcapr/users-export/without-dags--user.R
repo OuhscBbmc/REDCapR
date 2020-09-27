497a8b23a46553a019864a48e9b4269aca36ceb2
Will Beasley <wibeasley@hotmail.com>
2020-09-27 11:14:42 -0500

    file expectation for users-export
    ref #308

---
+++ inst/test-data/specific-redcapr/users-export/without-dags--user.R
@@ -0,0 +1,20 @@
+structure(list(username = c("unittestphifree", "wbeasleya"), 
+    email = c("wibeasley@hotmail.com", "william-beasley@ouhsc.edu"
+    ), firstname = c("Unit Test", "Will"), lastname = c("PHI Free", 
+    "Beasley_A"), expiration = structure(c(NA_real_, NA_real_
+    ), class = "Date"), data_access_group = c(NA_character_, 
+    NA_character_), data_access_group_id = c(NA_character_, NA_character_
+    ), design = c(FALSE, TRUE), user_rights = c(FALSE, TRUE), 
+    data_access_groups = c(FALSE, TRUE), data_export = c("1", 
+    "1"), reports = c(TRUE, TRUE), stats_and_charts = c(TRUE, 
+    TRUE), manage_survey_participants = c(TRUE, TRUE), calendar = c(TRUE, 
+    TRUE), data_import_tool = c(FALSE, TRUE), data_comparison_tool = c(FALSE, 
+    TRUE), logging = c(FALSE, TRUE), file_repository = c(TRUE, 
+    TRUE), data_quality_create = c(FALSE, TRUE), data_quality_execute = c(FALSE, 
+    TRUE), api_export = c(TRUE, FALSE), api_import = c(FALSE, 
+    FALSE), mobile_app = c(FALSE, FALSE), mobile_app_download_data = c(FALSE, 
+    FALSE), record_create = c(TRUE, TRUE), record_rename = c(FALSE, 
+    FALSE), record_delete = c(FALSE, FALSE), lock_records_all_forms = c(FALSE, 
+    FALSE), lock_records = c(FALSE, FALSE), lock_records_customization = c(FALSE, 
+    FALSE)), row.names = c(NA, -2L), class = c("tbl_df", "tbl", 
+"data.frame"))
---
+++ inst/test-data/specific-redcapr/users-export/without-dags--user_form.R
@@ -0,0 +1,7 @@
+structure(list(username = c("unittestphifree", "unittestphifree", 
+"unittestphifree", "wbeasleya", "wbeasleya", "wbeasleya"), form_name = c("demographics", 
+"health", "race_and_ethnicity", "demographics", "health", "race_and_ethnicity"
+), permission_id = c(1L, 1L, 1L, 1L, 1L, 1L), permission = structure(c(3L, 
+3L, 3L, 3L, 3L, 3L), .Label = c("no_access", "readonly", "edit_form", 
+"edit_survey", "unknown"), class = "factor")), row.names = c(NA, 
+-6L), class = c("tbl_df", "tbl", "data.frame"))
