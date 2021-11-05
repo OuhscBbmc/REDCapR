7dda3385f76a83a8a96907b76aa0e7f04796afa3
Will Beasley <wibeasley@hotmail.com>
2021-11-04 21:23:02 -0500

    test repeating instruments w/ sparse records
    ref #143

---
+++ inst/test-data/specific-redcapr/read-batch-repeating-sparse/default.R
@@ -0,0 +1,14 @@
+structure(list(record_id = c(1, 1, 1, 1, 2, 2, 3, 4, 5), redcap_repeat_instrument = c(NA, 
+"bp", "bp", "bp", NA, "bp", NA, NA, NA), redcap_repeat_instance = c(NA, 
+1, 2, 3, NA, 1, NA, NA, NA), date_enrolled = structure(c(18183, 
+NA, NA, NA, 17929, NA, 18935, 18935, NA), class = "Date"), first_name = c("aaa", 
+NA, NA, NA, "bbb", NA, "empty 3", "empty 4", "empty 5"), dob = structure(c(14896, 
+NA, NA, NA, 14642, NA, NA, NA, NA), class = "Date"), age = c(8.9, 
+NA, NA, NA, 9.6, NA, NA, NA, NA), ethnicity = c(1, NA, NA, NA, 
+0, NA, NA, NA, NA), race = c(4, NA, NA, NA, 6, NA, NA, NA, NA
+), sex = c(0, NA, NA, NA, 1, NA, NA, NA, NA), demographics_complete = c(2, 
+NA, NA, NA, 2, NA, 0, 0, 0), date_bp = structure(c(NA, 18183, 
+18183, 18183, NA, 12512, NA, NA, NA), class = "Date"), bp_systolic = c(NA, 
+110, 111, 112, NA, 114, NA, NA, NA), bp_diastolic = c(NA, 100, 
+101, 102, NA, 104, NA, NA, NA), bp_complete = c(NA, 2, 2, 2, 
+NA, 2, NA, NA, NA)), row.names = c(NA, -9L), class = "data.frame")
