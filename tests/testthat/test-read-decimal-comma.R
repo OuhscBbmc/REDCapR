05837633a60bde4a8974ccf21943afe9420c7226
Will Beasley <wibeasley@hotmail.com>
2021-11-26 11:21:33 -0600

    starting to test mismatched decmial commas
    ref #366

---
+++ tests/testthat/test-read-decimal-comma.R
@@ -0,0 +1,34 @@
+library(testthat)
+
+credential  <- retrieve_credential_testing(2630L)
+update_expectation  <- FALSE
+
+test_that("default-mismatched", {
+  testthat::skip_on_cran()
+  path_expected <- "test-data/decimal-comma/default-mismatched.R"
+  expected_outcome_message <- "\\d+ records and \\d+ columns were read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\."
+
+  expect_message(
+    returned_object <-
+      redcap_read_oneshot(
+        credential$redcap_uri,
+        credential$token
+      ),
+    regexp = expected_outcome_message
+  )
+
+  if (update_expectation) save_expected(returned_object$data, path_expected)
+  expected_data_frame <- retrieve_expected(path_expected)
+
+  expect_equal(returned_object$data, expected=expected_data_frame, label="The returned data.frame should be correct", ignore_attr = TRUE) # dput(returned_object$data)
+  expect_equal(returned_object$status_code, expected=200L)
+  expect_equal(returned_object$raw_text, expected="", ignore_attr = TRUE) # dput(returned_object$raw_text)
+  expect_true(returned_object$records_collapsed=="", "A subset of records was not requested.")
+  expect_true(returned_object$fields_collapsed=="", "A subset of fields was not requested.")
+  expect_true(returned_object$filter_logic=="", "A filter was not specified.")
+  expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
+  expect_true(returned_object$success)
+})
+
+
+rm(credential)
