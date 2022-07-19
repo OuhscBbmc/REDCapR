e7aa6ff2b9a96ea9dcd40b7333bb1e5a5072bd18
Will Beasley <wibeasley@hotmail.com>
2022-07-19 12:17:29 -0500

    check incoming parameters to kernel_api
    ref #396

--- R/kernel-api.R
+++ R/kernel-api.R
@@ -46,7 +46,15 @@ kernel_api <- function(
   content_type        = "text/csv"
   ) {
 
+  checkmate::assert_character(redcap_uri    , len = 1, any.missing = FALSE, null.ok = FALSE)
+  checkmate::assert_character(encoding      , len = 1, any.missing = FALSE, null.ok = FALSE)
+  checkmate::assert_character(content_type  , len = 1, any.missing = FALSE, null.ok = FALSE)
+
   start_time <- Sys.time()
+
+  # if (httr::http_error(redcap_uri)) {
+  #   stop("The url `", redcap_uri, "` is not found or throws an error.")
+  # }
 
   result <- httr::POST(
     url     = redcap_uri,
