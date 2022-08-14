16f2b38b42af77ce91cf20ce68f19f3ee7434546
Will Beasley <wibeasley@hotmail.com>
2022-08-13 23:35:22 -0500

    allow token regex env variable
    close #370

--- NEWS.md
+++ NEWS.md
@@ -5,6 +5,7 @@ Upcoming Release
 
 * New `redcap_log_read()` function.  Exports a project's log.  (Thanks @joundso, #383, #320)
 * New parameter `blank_for_gray_form_status` in the functions `redcap_read()`, `redcap_read_oneshot()`, and `redcap_read_oneshot_eav()`. (@greg-botwin, #386, #389)
+* `sanitized_token()` now accepts an alternative regex pattern.  (Suggested by @maeon & @michalkouril, #370)
 
 ### Minor Enhancements
 
--- tests/testthat/test-sanitize.R
+++ tests/testthat/test-sanitize.R
@@ -20,7 +20,7 @@ test_that("sanitize token w/o line endings", {
   secret_token <- "12345678901234567"
   expect_error(
     object    = sanitize_token(secret_token),
-    regexp    = "The token is not a valid 32-character hexademical value\\."
+    regexp    = "^The token does not conform with the regex."
   )
 })
 
@@ -28,7 +28,7 @@ test_that("sanitize token -NA", {
   secret_token <- NA_character_
   expect_error(
     object    = sanitize_token(secret_token),
-    regexp    = "The token is `NA`, not a valid 32-character hexademical value\\."
+    regexp    = "The token is `NA`, which is not allowed\\."
   )
 })
 
@@ -36,7 +36,7 @@ test_that("sanitize token -empty", {
   secret_token <- ""
   expect_error(
     object    = sanitize_token(secret_token),
-    regexp    = "The token is an empty string, not a valid 32-character hexademical value\\."
+    regexp    = "The token is an empty string, which is not allowed\\."
   )
 })
 
@@ -48,3 +48,26 @@ test_that("sanitize token - lowercase (#347)", {
   # No change
   expect_equal(returned, secret_token)
 })
+
+# test_that("sanitize token - env variable -success", {
+#   Sys.setenv("REDCAP_TOKEN_PATTERN" = "^([A-Za-z\\d+/\\+=]{10})$")
+#   base::on.exit(Sys.unsetenv("REDCAP_TOKEN_PATTERN"))
+#
+#   secret_token <- "abcde1234="
+#   returned <- REDCapR::sanitize_token(secret_token)
+#
+#   # No change
+#   expect_equal(returned, secret_token)
+#   Sys.getenv("REDCAP_TOKEN_PATTERN")
+# })
+# test_that("sanitize token - env variable -bad", {
+#   Sys.setenv("REDCAP_TOKEN_PATTERN" = "^([A-Za-z\\d+/\\+=]{10})$")
+#   base::on.exit(Sys.unsetenv("REDCAP_TOKEN_PATTERN"))
+#
+#   secret_token <- "abcde12341234="
+#
+#   expect_error(
+#     REDCapR::sanitize_token(secret_token),
+#     "^The token does not conform with the regex"
+#   )
+# })
