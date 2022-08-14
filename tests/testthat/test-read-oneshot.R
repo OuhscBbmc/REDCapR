76b1784b4acd3450dc0b06880a4c8df661e970aa
Will Beasley <wibeasley@hotmail.com>
2022-08-13 19:01:40 -0500

    adding tests for blank_for_gray
    ref #389

--- tests/testthat/test-read-oneshot.R
+++ tests/testthat/test-read-oneshot.R
@@ -414,6 +414,60 @@ test_that("filter-character", {
   expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
   expect_true(returned_object$success)
 })
+test_that("blank-for-gray-status-true", {
+  testthat::skip_on_cran()
+  credential_blank_for_gray  <- retrieve_credential_testing(3003L)
+  path_expected <- "test-data/specific-redcapr/read-oneshot/blank-for-gray-true.R"
+  expected_outcome_message <- "\\d+ records and \\d+ columns were read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\."
+
+  expect_message(
+    regexp          = expected_outcome_message,
+    returned_object <- redcap_read_oneshot(
+      redcap_uri    = credential_blank_for_gray$redcap_uri,
+      token         = credential_blank_for_gray$token,
+      export_blankforgray_form_status = TRUE
+    )
+  )
+
+  if (update_expectation) save_expected(returned_object$data, path_expected)
+  expected_data_frame <- retrieve_expected(path_expected)
+
+  expect_equal(returned_object$data, expected=expected_data_frame, label="The returned data.frame should be correct", ignore_attr = TRUE) # dput(returned_object$data)
+  expect_equal(returned_object$status_code, expected=200L)
+  expect_equal(returned_object$raw_text, expected="", ignore_attr = TRUE) # dput(returned_object$raw_text)
+  expect_true(returned_object$records_collapsed=="", "A subset of records was not requested.")
+  expect_true(returned_object$fields_collapsed=="", "A subset of fields was not requested.")
+  expect_true(returned_object$filter_logic=="", "A filter was not specified.")
+  expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
+  expect_true(returned_object$success)
+})
+test_that("blank-for-gray-status-false", {
+  testthat::skip_on_cran()
+  credential_blank_for_gray  <- retrieve_credential_testing(3003L)
+  path_expected <- "test-data/specific-redcapr/read-oneshot/blank-for-gray-false.R"
+  expected_outcome_message <- "\\d+ records and \\d+ columns were read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\."
+
+  expect_message(
+    regexp          = expected_outcome_message,
+    returned_object <- redcap_read_oneshot(
+      redcap_uri    = credential_blank_for_gray$redcap_uri,
+      token         = credential_blank_for_gray$token,
+      export_blankforgray_form_status = FALSE
+    )
+  )
+
+  if (update_expectation) save_expected(returned_object$data, path_expected)
+  expected_data_frame <- retrieve_expected(path_expected)
+
+  expect_equal(returned_object$data, expected=expected_data_frame, label="The returned data.frame should be correct", ignore_attr = TRUE) # dput(returned_object$data)
+  expect_equal(returned_object$status_code, expected=200L)
+  expect_equal(returned_object$raw_text, expected="", ignore_attr = TRUE) # dput(returned_object$raw_text)
+  expect_true(returned_object$records_collapsed=="", "A subset of records was not requested.")
+  expect_true(returned_object$fields_collapsed=="", "A subset of fields was not requested.")
+  expect_true(returned_object$filter_logic=="", "A filter was not specified.")
+  expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
+  expect_true(returned_object$success)
+})
 
 test_that("date-range", {
   testthat::skip_on_cran()
