0989b36e88b2ff680644dd747d06f0c072b2efbb
Will Beasley <wibeasley@hotmail.com>
2022-08-14 14:22:34 -0500

    turn sanitize checks back on
    I don't know why I commented them out last night
    
    ref #370

--- tests/testthat/test-sanitize.R
+++ tests/testthat/test-sanitize.R
@@ -49,25 +49,25 @@ test_that("sanitize token - lowercase (#347)", {
   expect_equal(returned, secret_token)
 })
 
-# test_that("sanitize token - env variable -success", {
-#   Sys.setenv("REDCAP_TOKEN_PATTERN" = "^([A-Za-z\\d+/\\+=]{10})$")
-#   base::on.exit(Sys.unsetenv("REDCAP_TOKEN_PATTERN"))
-#
-#   secret_token <- "abcde1234="
-#   returned <- REDCapR::sanitize_token(secret_token)
-#
-#   # No change
-#   expect_equal(returned, secret_token)
-#   Sys.getenv("REDCAP_TOKEN_PATTERN")
-# })
-# test_that("sanitize token - env variable -bad", {
-#   Sys.setenv("REDCAP_TOKEN_PATTERN" = "^([A-Za-z\\d+/\\+=]{10})$")
-#   base::on.exit(Sys.unsetenv("REDCAP_TOKEN_PATTERN"))
-#
-#   secret_token <- "abcde12341234="
-#
-#   expect_error(
-#     REDCapR::sanitize_token(secret_token),
-#     "^The token does not conform with the regex"
-#   )
-# })
+test_that("sanitize token - env variable -success", {
+  Sys.setenv("REDCAP_TOKEN_PATTERN" = "^([A-Za-z\\d+/\\+=]{10})$")
+  base::on.exit(Sys.unsetenv("REDCAP_TOKEN_PATTERN"))
+
+  secret_token <- "abcde1234="
+  returned <- REDCapR::sanitize_token(secret_token)
+
+  # No change
+  expect_equal(returned, secret_token)
+  Sys.getenv("REDCAP_TOKEN_PATTERN")
+})
+test_that("sanitize token - env variable -bad", {
+  Sys.setenv("REDCAP_TOKEN_PATTERN" = "^([A-Za-z\\d+/\\+=]{10})$")
+  base::on.exit(Sys.unsetenv("REDCAP_TOKEN_PATTERN"))
+
+  secret_token <- "abcde12341234="
+
+  expect_error(
+    REDCapR::sanitize_token(secret_token),
+    "^The token does not conform with the regex"
+  )
+})
