b4a7bf549abb0f6f99a2eb43cbf50f316b7c540d
Will Beasley <wibeasley@hotmail.com>
2022-08-21 21:27:59 -0500

    cronjob hack for CoPH
    @thomasnwilson, now the other REDCap server is complaining that the cronjob isn't running

--- playgrounds/cron-trigger-hack.R
+++ playgrounds/cron-trigger-hack.R
@@ -1,4 +1,5 @@
 # Use this hack if the cronjob isn't running properly on the server,
 #    But the cronjob link is working (see bottom of https://bbmc.ouhsc.edu/redcap/redcap_v10.5.1/ControlCenter/cron_jobs.php)
 
-httr::GET("https://bbmc.ouhsc.edu/redcap/cron.php")
+httr::GET("https://redcap.ouhsc.edu/redcap/cron.php")
+# httr::GET("https://bbmc.ouhsc.edu/redcap/cron.php")
