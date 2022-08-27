59d1f8236ee743ddc8f7f79af946f2fdc76d5f17
Will Beasley <wibeasley@hotmail.com>
2022-08-27 10:44:18 -0500

    include explanations in the right-side comments
    ref #405

--- playgrounds/metadata-reflect-coltypes.R
+++ playgrounds/metadata-reflect-coltypes.R
@@ -1,3 +1,4 @@
+import::from("magrittr", "%>%")
 
 uri   <- "https://bbmc.ouhsc.edu/redcap/api/"
 token <- "9A81268476645C4E5F03428B8AC3AA7B"
@@ -11,62 +12,67 @@ d |>
     vt            = text_validation_type_or_show_slider_number,
   ) |>
   dplyr::mutate(
-    readr_col_type =
+    response =
       dplyr::case_when(
-        field_type == "truefalse"         ~ "col_logical()",
-        field_type == "radio"             ~ "col_character()",
-        field_type == "file"              ~ "col_character()",
-
-        vt == "alpha_only"                ~ "col_character()",
-        vt == "date_dmy"                  ~ "col_date()",
-        vt == "date_mdy"                  ~ "col_date()",
-        vt == "date_ymd"                  ~ "col_date()",
-        vt == "datetime_dmy"              ~ "col_datetime(\"%Y-%m-%d %H:%M\")",
-        vt == "datetime_mdy"              ~ "col_datetime(\"%Y-%m-%d %H:%M\")",
-        vt == "datetime_seconds_dmy"      ~ "col_datetime(\"%Y-%m-%d %H:%M:%S\")",
-        vt == "datetime_seconds_mdy"      ~ "col_datetime(\"%Y-%m-%d %H:%M:%S\")",
-        vt == "datetime_seconds_ymd"      ~ "col_datetime(\"%Y-%m-%d %H:%M:%S\")",
-        vt == "datetime_ymd"              ~ "col_datetime(\"%Y-%m-%d %H:%M\")",
-        vt == "email"                     ~ "col_character()",
-        vt == "integer"                   ~ "col_integer()",
-        vt == "mrn_10d"                   ~ "col_character()",
-        vt == "mrn_generic"               ~ "col_character()",
-        vt == "number"                    ~ "col_double()",
-        vt == "number_1dp"                ~ "col_double()",
-        vt == "number_1dp_comma_decimal"  ~ "col_double()",
-        vt == "number_2dp"                ~ "col_double()",
-        vt == "number_2dp_comma_decimal"  ~ "col_double()",
-        vt == "number_3dp"                ~ "col_double()",
-        vt == "number_3dp_comma_decimal"  ~ "col_double()",
-        vt == "number_4dp"                ~ "col_double()",
-        vt == "number_4dp_comma_decimal"  ~ "col_double()",
-        vt == "number_comma_decimal"      ~ "col_double()",
-        vt == "phone"                     ~ "col_character()",
-        vt == "phone_australia"           ~ "col_character()",
-        vt == "postalcode_australia"      ~ "col_character()",
-        vt == "postalcode_canada"         ~ "col_character()",
-        vt == "postalcode_french"         ~ "col_character()",
-        vt == "postalcode_germany"        ~ "col_character()",
-        vt == "ssn"                       ~ "col_character()",
-        vt == "time"                      ~ "col_time(\"%H:%M\")",
-        vt == "time_mm_ss"                ~ "col_time(\"%H:%M\")",
-        vt == "vmrn"                      ~ "col_character()",
-        vt == "zipcode"                   ~ "col_character()",
-        TRUE                              ~ "col_character()"
+        field_type == "truefalse"         ~ paste0("col_logical()"                        , "||field_type is truefalse"),
+        field_type == "radio"             ~ paste0("col_character()"                      , "||field_type is radio"),
+        field_type == "file"              ~ paste0("col_character()"                      , "||field_type is file"),
+        field_type == "text" & is.na(vt)  ~ paste0("col_character()"                      , "||field_type is text and validation is ''"),
+        field_type == "text" & vt == ""   ~ paste0("col_character()"                      , "||field_type is text and validation is ''"),
+        vt == "alpha_only"                ~ paste0("col_character()"                      , "||validation is alpha_only"),
+        vt == "date_dmy"                  ~ paste0("col_date()"                           , "||validation is date_dmy"),
+        vt == "date_mdy"                  ~ paste0("col_date()"                           , "||validation is date_mdy"),
+        vt == "date_ymd"                  ~ paste0("col_date()"                           , "||validation is date_ymd"),
+        vt == "datetime_dmy"              ~ paste0("col_datetime(\"%Y-%m-%d %H:%M\")"     , "||validation is datetime_dmy"),
+        vt == "datetime_mdy"              ~ paste0("col_datetime(\"%Y-%m-%d %H:%M\")"     , "||validation is datetime_mdy"),
+        vt == "datetime_seconds_dmy"      ~ paste0("col_datetime(\"%Y-%m-%d %H:%M:%S\")"  , "||validation is datetime_seconds_dmy"),
+        vt == "datetime_seconds_mdy"      ~ paste0("col_datetime(\"%Y-%m-%d %H:%M:%S\")"  , "||validation is datetime_seconds_mdy"),
+        vt == "datetime_seconds_ymd"      ~ paste0("col_datetime(\"%Y-%m-%d %H:%M:%S\")"  , "||validation is datetime_seconds_ymd"),
+        vt == "datetime_ymd"              ~ paste0("col_datetime(\"%Y-%m-%d %H:%M\")"     , "||validation is datetime_ymd"),
+        vt == "email"                     ~ paste0("col_character()"                      , "||validation is email"),
+        vt == "integer"                   ~ paste0("col_integer()"                        , "||validation is integer"),
+        vt == "mrn_10d"                   ~ paste0("col_character()"                      , "||validation is mrn_10d"),
+        vt == "mrn_generic"               ~ paste0("col_character()"                      , "||validation is mrn_generic"),
+        vt == "number"                    ~ paste0("col_double()"                         , "||validation is number"),
+        vt == "number_1dp"                ~ paste0("col_double()"                         , "||validation is number_1dp"),
+        vt == "number_1dp_comma_decimal"  ~ paste0("col_double()"                         , "||validation is number_1dp_comma_decimal"),
+        vt == "number_2dp"                ~ paste0("col_double()"                         , "||validation is number_2dp"),
+        vt == "number_2dp_comma_decimal"  ~ paste0("col_double()"                         , "||validation is number_2dp_comma_decimal"),
+        vt == "number_3dp"                ~ paste0("col_double()"                         , "||validation is number_3dp"),
+        vt == "number_3dp_comma_decimal"  ~ paste0("col_double()"                         , "||validation is number_3dp_comma_decimal"),
+        vt == "number_4dp"                ~ paste0("col_double()"                         , "||validation is number_4dp"),
+        vt == "number_4dp_comma_decimal"  ~ paste0("col_double()"                         , "||validation is number_4dp_comma_decimal"),
+        vt == "number_comma_decimal"      ~ paste0("col_double()"                         , "||validation is number_comma_decimal"),
+        vt == "phone"                     ~ paste0("col_character()"                      , "||validation is phone"),
+        vt == "phone_australia"           ~ paste0("col_character()"                      , "||validation is phone_australia"),
+        vt == "postalcode_australia"      ~ paste0("col_character()"                      , "||validation is postalcode_australia"),
+        vt == "postalcode_canada"         ~ paste0("col_character()"                      , "||validation is postalcode_canada"),
+        vt == "postalcode_french"         ~ paste0("col_character()"                      , "||validation is postalcode_french"),
+        vt == "postalcode_germany"        ~ paste0("col_character()"                      , "||validation is postalcode_germany"),
+        vt == "ssn"                       ~ paste0("col_character()"                      , "||validation is ssn"),
+        vt == "time"                      ~ paste0("col_time(\"%H:%M\")"                  , "||validation is time"),
+        vt == "time_mm_ss"                ~ paste0("col_time(\"%H:%M\")"                  , "||validation is time_mm_ss"),
+        vt == "vmrn"                      ~ paste0("col_character()"                      , "||validation is vmrn"),
+        vt == "zipcode"                   ~ paste0("col_character()"                      , "||validation is zipcode"),
+        TRUE                              ~ paste0("col_character()"                      , "||no condition was recognized"),
       )
   ) |>
   dplyr::mutate(
-    suggestion  = paste0("readr::", readr_col_type),
+    readr_col_type  = sub("^(col_.+)\\|\\|(.+)$", "\\1", .data$response),
+    explanation     = sub("^(col_.+)\\|\\|(.+)$", "\\2", .data$response),
 
     # Calculate the odd number of spaces -just beyond the longest variable name.
-    padding = nchar(.data$field_name),
-    padding = max(.data$padding) %/% 2 * 2 + 3,
+    padding1  = nchar(.data$field_name),
+    padding1  = max(.data$padding1) %/% 2 * 2 + 3,
+    padding2  = nchar(.data$readr_col_type),
+    padding2  = max(.data$padding2) %/% 2 * 2 + 3,
 
     # Pad the left side before appending the right side.
-    aligned = sprintf("  %-*s = readr::%s", .data$padding, .data$field_name, .data$readr_col_type)
+    aligned = sprintf("  %-*s = readr::%-*s, # b/c %s", .data$padding1, .data$field_name, .data$padding2, .data$readr_col_type, .data$explanation)
   ) |>
+  # dplyr::pull(.data$response) |>
   dplyr::pull(.data$aligned) |>
-  paste(collapse = ",\n") %>%
+  paste(collapse = "\n") %>%
   # I'd prefer this approach, but the `.` is causing problems with R CMD check.
   paste0(
     "col_types <- readr::cols_only(\n",
