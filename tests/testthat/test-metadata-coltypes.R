be2407610c2fccdf3e24f785fa06cf18964bc31f
Will Beasley <wibeasley@hotmail.com>
2022-08-30 19:36:58 -0500

    test repeating instruments
    ref #405

--- tests/testthat/test-metadata-coltypes.R
+++ tests/testthat/test-metadata-coltypes.R
@@ -26,6 +26,12 @@ test_that("simple", {
 
   expect_equal(actual, expected=expected, label="The returned col_types should be correct", ignore_attr = TRUE) # dput(returned_object$data)
   expect_s3_class(actual, "col_spec")
+
+  ds <- redcap_read_oneshot(credential$redcap_uri, credential$token)$data
+  col_metadata <- names(actual[[1]])
+  col_data     <- colnames(ds)
+
+  setdiff(col_data, col_metadata)
 })
 
 test_that("longitudinal", {
@@ -44,4 +50,34 @@ test_that("longitudinal", {
 
   expect_equal(actual, expected=expected, label="The returned col_types should be correct", ignore_attr = TRUE) # dput(returned_object$data)
   expect_s3_class(actual, "col_spec")
+
+  ds <- redcap_read_oneshot(credential$redcap_uri, credential$token)$data
+  col_metadata <- names(actual[[1]])
+  col_data     <- colnames(ds)
+
+  setdiff(col_data, col_metadata)
+})
+
+test_that("repeating-instruments", {
+  testthat::skip_on_cran()
+  credential               <- retrieve_credential_testing(1400L)
+  path_expected            <- "test-data/specific-redcapr/metadata-coltypes/repeating-instruments.R"
+  expected_outcome_message <- "col_types <- readr::cols"
+
+  expect_message(
+    regexp  = expected_outcome_message,
+    actual  <- redcap_metadata_coltypes(credential$redcap_uri, credential$token)
+  )
+
+  if (update_expectation) save_expected(actual, path_expected)
+  expected <- retrieve_expected(path_expected)
+
+  expect_equal(actual, expected=expected, label="The returned col_types should be correct", ignore_attr = TRUE) # dput(returned_object$data)
+  expect_s3_class(actual, "col_spec")
+
+  ds <- redcap_read_oneshot(credential$redcap_uri, credential$token)$data
+  col_metadata <- names(actual[[1]])
+  col_data     <- colnames(ds)
+
+  setdiff(col_data, col_metadata)
 })
