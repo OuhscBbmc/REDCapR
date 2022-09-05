4c82e12d1622aacf5c60da4912a656dcb1fde03d
Will Beasley <wibeasley@hotmail.com>
2022-09-05 18:32:49 -0500

    catch bad token sooner
    ref #415

--- R/redcap-read.R
+++ R/redcap-read.R
@@ -273,13 +273,13 @@ redcap_read <- function(
     config_options     = config_options
   )
 
-  # if (!metadata$success) {
-  #   error_message     <- sprintf(
-  #     "The REDCapR record export operation was not successful.  The error message was:\n%s",
-  #     metadata$raw_text
-  #   )
-  #   stop(error_message)
-  # }
+  if (!metadata$success) {
+    error_message     <- sprintf(
+      "The REDCapR record export operation was not successful.  The error message was:\n%s",
+      metadata$raw_text
+    )
+    stop(error_message)
+  }
 
   initial_call <- REDCapR::redcap_read_oneshot(
     redcap_uri                 = redcap_uri,
