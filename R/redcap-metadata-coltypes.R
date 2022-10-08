68737cb3a4113dc02adfc71e8c7ad721c1b96502
Will Beasley <wibeasley@hotmail.com>
2022-10-08 12:41:27 -0500

    add boolean `plumbing` variable to internal metadata fx
    ref #439

--- R/redcap-metadata-coltypes.R
+++ R/redcap-metadata-coltypes.R
@@ -106,7 +106,7 @@
 #' the project manager selected the "integer" validation option, all those
 #' "abcd" values remain untouched.
 #'
-#' This is one reason `redcap_metadata_coltypes` prints it suggestions to the console.
+#' This is one reason `redcap_metadata_coltypes()` prints it suggestions to the console.
 #' It allows the developer to adjust the specifications to match the values
 #' returned by the API.  The the "abcd" scenario, consider (a) changing the type
 #' from `col_integer` to `col_character`, (b) excluding the trash values,
@@ -344,8 +344,14 @@ redcap_metadata_internal <- function(
     dplyr::ungroup() %>%
     dplyr::arrange(.data$form_order,  .data$field_order_within_form) %>%
     dplyr::select(-.data$form_order, -.data$field_order_within_form) %>%
-    tibble::add_row(d_again, .after = 1)
+    tibble::add_row(d_again, .after = 1) %>%
+    dplyr::mutate(
+      plumbing = (.data$field_name %in% c("redcap_event_name", "redcap_repeat_instrument", "redcap_repeat_instance"))
+    )
 
+  d_meta$plumbing[1] <- TRUE # The first field should always be the "record" identifier.
+
+  # The types of variables that are in metadata, but NOT variables:
   # setdiff(d_meta$field_name_base, d_var$original_field_name)
   # [1] "signature"   "file_upload" "descriptive"
 
@@ -451,7 +457,8 @@ redcap_metadata_internal <- function(
       # .data$padding1,
       # .data$padding2,
       .data$aligned,
-      .data$field_name_base
+      .data$field_name_base,
+      .data$plumbing
     )
 
   decimal_period_any <- any(d_meta$vt %in% c("number", "number_1dp", "number_2dp", "number_3dp", "number_4dp" ))
@@ -476,3 +483,14 @@ redcap_metadata_internal <- function(
     repeating       = d_proj$has_repeating_instruments_or_events[1]
   )
 }
+
+# uri   <- "https://bbmc.ouhsc.edu/redcap/api/"
+#
+# A simple project (pid 153)
+# REDCapR:::redcap_metadata_internal(uri, "9A81268476645C4E5F03428B8AC3AA7B")$d_variable
+#
+# A longitudinal project (pid 212)
+# REDCapR:::redcap_metadata_internal(uri, "0434F0E9CF53ED0587847AB6E51DE762")$d_variable
+#
+# # A repeating measures (pid 3181)
+# REDCapR:::redcap_metadata_internal(uri, "22C3FF1C8B08899FB6F86D91D874A159")$d_variable
