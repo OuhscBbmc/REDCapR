2339468c2e15f728862f8ff736e2103adf5473de
Will Beasley <wibeasley@hotmail.com>
2022-10-22 16:31:01 -0500

    reorder normal test

--- tests/testthat/test-metadata-read.R
+++ tests/testthat/test-metadata-read.R
@@ -20,6 +20,32 @@ test_that("Smoke Test", {
   expect_type(returned_object, "list")
 })
 
+test_that("normal", {
+  testthat::skip_on_cran()
+  path_expected <- "test-data/specific-redcapr/metadata-read/normal.R"
+  expected_outcome_message <- "The data dictionary describing \\d+ fields was read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\.  The http status code was 200."
+
+  returned_object <-
+    redcap_metadata_read(
+      redcap_uri  = credential$redcap_uri,
+      token       = credential$token,
+      verbose     = FALSE
+    )
+
+  if (update_expectation) save_expected(returned_object$data, path_expected)
+  expected_data_frame <- retrieve_expected(path_expected)
+
+  # datapasta::tribble_paste(returned_object$data)
+  expect_equal(returned_object$data, expected=expected_data_frame, label="The returned data.frame should be correct") # dput(returned_object$data)
+  expect_equal(returned_object$status_code, expected=200L)
+  expect_equal(returned_object$raw_text, expected="", ignore_attr = TRUE) # dput(returned_object$raw_text)
+  expect_true(returned_object$forms_collapsed=="", "A subset of forms was not requested.")
+  expect_true(returned_object$fields_collapsed=="", "A subset of fields was not requested.")
+  expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
+  expect_true(returned_object$success)
+  expect_s3_class(returned_object$data, "tbl")
+})
+
 test_that("Super-wide", {
   testthat::skip_on_cran()
   expected_outcome_message <- "The data dictionary describing 3,001 fields was read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\.  The http status code was 200\\."
@@ -97,32 +123,6 @@ test_that("Problematic Dictionary", {
   expect_equal(nrow(returned_object$data), expected=expected_row_count) # dput(returned_object$data)
   expect_equal(ncol(returned_object$data), expected=expected_column_count)
   expect_equal(sum(is.na(returned_object$data)), expected=expected_na_cells)
-  expect_s3_class(returned_object$data, "tbl")
-})
-
-test_that("normal", {
-  testthat::skip_on_cran()
-  path_expected <- "test-data/specific-redcapr/metadata-read/normal.R"
-  expected_outcome_message <- "The data dictionary describing \\d+ fields was read from REDCap in \\d+(\\.\\d+\\W|\\W)seconds\\.  The http status code was 200."
-
-  returned_object <-
-    redcap_metadata_read(
-      redcap_uri  = credential$redcap_uri,
-      token       = credential$token,
-      verbose     = FALSE
-    )
-
-  if (update_expectation) save_expected(returned_object$data, path_expected)
-  expected_data_frame <- retrieve_expected(path_expected)
-
-  # datapasta::tribble_paste(returned_object$data)
-  expect_equal(returned_object$data, expected=expected_data_frame, label="The returned data.frame should be correct") # dput(returned_object$data)
-  expect_equal(returned_object$status_code, expected=200L)
-  expect_equal(returned_object$raw_text, expected="", ignore_attr = TRUE) # dput(returned_object$raw_text)
-  expect_true(returned_object$forms_collapsed=="", "A subset of forms was not requested.")
-  expect_true(returned_object$fields_collapsed=="", "A subset of fields was not requested.")
-  expect_match(returned_object$outcome_message, regexp=expected_outcome_message, perl=TRUE)
-  expect_true(returned_object$success)
   expect_s3_class(returned_object$data, "tbl")
 })
 
