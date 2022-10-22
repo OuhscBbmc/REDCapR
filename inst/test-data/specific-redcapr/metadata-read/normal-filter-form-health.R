86b7eb7edfd632c04e6bae6327d8657f0b8d039e
Will Beasley <wibeasley@hotmail.com>
2022-10-22 17:42:37 -0500

    tests for filtering forms of metadata
    ref #445

---
+++ inst/test-data/specific-redcapr/metadata-read/normal-filter-form-health.R
@@ -0,0 +1,25 @@
+structure(list(field_name = c("height", "weight", "bmi", "comments", 
+"mugshot"), form_name = c("health", "health", "health", "health", 
+"health"), section_header = c(NA, NA, NA, "General Comments", 
+NA), field_type = c("text", "text", "calc", "notes", "file"), 
+    field_label = c("Height (cm)", "Weight (kilograms)", "BMI", 
+    "Comments", "Mugshot"), select_choices_or_calculations = c(NA, 
+    NA, "round(([weight]*10000)/(([height])^(2)),1)", NA, NA), 
+    field_note = c(NA_character_, NA_character_, NA_character_, 
+    NA_character_, NA_character_), text_validation_type_or_show_slider_number = c("number", 
+    "integer", NA, NA, NA), text_validation_min = c("130", "35", 
+    NA, NA, NA), text_validation_max = c("215", "200", NA, NA, 
+    NA), identifier = c(NA_character_, NA_character_, NA_character_, 
+    NA_character_, NA_character_), branching_logic = c(NA_character_, 
+    NA_character_, NA_character_, NA_character_, NA_character_
+    ), required_field = c(NA_character_, NA_character_, NA_character_, 
+    NA_character_, NA_character_), custom_alignment = c(NA_character_, 
+    NA_character_, NA_character_, NA_character_, NA_character_
+    ), question_number = c(NA_character_, NA_character_, NA_character_, 
+    NA_character_, NA_character_), matrix_group_name = c(NA_character_, 
+    NA_character_, NA_character_, NA_character_, NA_character_
+    ), matrix_ranking = c(NA_character_, NA_character_, NA_character_, 
+    NA_character_, NA_character_), field_annotation = c(NA_character_, 
+    NA_character_, NA_character_, NA_character_, NA_character_
+    )), row.names = c(NA, -5L), class = c("tbl_df", "tbl", "data.frame"
+))
