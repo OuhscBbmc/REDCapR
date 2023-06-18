4b6a6aa4cf4a90d50ed3dee374b27ff872c75621
Will Beasley <wibeasley@hotmail.com>
2023-06-17 22:23:12 -0500

    Update without-dags--user.R
    part of upgrade from 13.6 to 13.7.1

--- inst/test-data/specific-redcapr/users-export/without-dags--user.R
+++ inst/test-data/specific-redcapr/users-export/without-dags--user.R
@@ -4,9 +4,9 @@ structure(list(username = c("unittestphifree", "wbeasleya"),
     "Beasley_A"), expiration = structure(c(NA_real_, NA_real_
     ), class = "Date"), data_access_group = c(NA_character_, 
     NA_character_), data_access_group_id = c(NA_character_, NA_character_
-    ), design = c(FALSE, TRUE), user_rights = c(FALSE, TRUE), 
-    data_access_groups = c(FALSE, TRUE), reports = c(TRUE, TRUE
-    ), stats_and_charts = c(TRUE, TRUE), manage_survey_participants = c(TRUE, 
+    ), design = c(FALSE, TRUE), alerts = c(0, 1), user_rights = c(FALSE, 
+    TRUE), data_access_groups = c(FALSE, TRUE), reports = c(TRUE, 
+    TRUE), stats_and_charts = c(TRUE, TRUE), manage_survey_participants = c(TRUE, 
     TRUE), calendar = c(TRUE, TRUE), data_import_tool = c(FALSE, 
     TRUE), data_comparison_tool = c(FALSE, TRUE), logging = c(TRUE, 
     TRUE), file_repository = c(TRUE, TRUE), data_quality_create = c(FALSE, 
@@ -15,7 +15,6 @@ structure(list(username = c("unittestphifree", "wbeasleya"),
     FALSE), mobile_app_download_data = c(FALSE, FALSE), record_create = c(TRUE, 
     TRUE), record_rename = c(FALSE, FALSE), record_delete = c(FALSE, 
     FALSE), lock_records_all_forms = c(FALSE, FALSE), lock_records = c(FALSE, 
-    FALSE), lock_records_customization = c(FALSE, FALSE), mycap_participants = c(0, 
-    0), forms_export = c("demographics:1,health:1,race_and_ethnicity:1", 
+    FALSE), lock_records_customization = c(FALSE, FALSE), forms_export = c("demographics:1,health:1,race_and_ethnicity:1", 
     "demographics:1,health:1,race_and_ethnicity:1")), row.names = c(NA, 
 -2L), class = c("tbl_df", "tbl", "data.frame"))
