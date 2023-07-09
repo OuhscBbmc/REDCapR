1a5262cdc7bd7741fb25ef8b80e1c61813385480
Will Beasley <wibeasley@hotmail.com>
2023-07-08 22:23:02 -0500

    align
    ref #485

--- tests/testthat/test-validate-uniqueness.R
+++ tests/testthat/test-validate-uniqueness.R
@@ -3,24 +3,24 @@ library(testthat)
 test_that("validate_uniqueness -good -all four", {
   d <- tibble::tribble(
     ~record_id, ~redcap_event_name, ~redcap_repeat_instrument, ~redcap_repeat_instance,
-    1L,"e1", "i1", 1L,
-    1L,"e1", "i1", 2L,
-    1L,"e1", "i1", 3L,
-    1L,"e1", "i1", 4L,
-    1L,"e1", "i2", 1L,
-    1L,"e1", "i2", 2L,
-    1L,"e1", "i2", 3L,
-    1L,"e1", "i2", 4L,
-    1L,"e2", "i1", 1L,
-    1L,"e2", "i1", 2L,
-    1L,"e2", "i1", 3L,
-    1L,"e2", "i1", 4L,
-    1L,"e2", "i2", 1L,
-    1L,"e2", "i2", 2L,
-    2L,"e1", "i1", 1L,
-    2L,"e1", "i1", 2L,
-    2L,"e1", "i1", 3L,
-    2L,"e1", "i1", 4L,
+    1L, "e1", "i1", 1L,
+    1L, "e1", "i1", 2L,
+    1L, "e1", "i1", 3L,
+    1L, "e1", "i1", 4L,
+    1L, "e1", "i2", 1L,
+    1L, "e1", "i2", 2L,
+    1L, "e1", "i2", 3L,
+    1L, "e1", "i2", 4L,
+    1L, "e2", "i1", 1L,
+    1L, "e2", "i1", 2L,
+    1L, "e2", "i1", 3L,
+    1L, "e2", "i1", 4L,
+    1L, "e2", "i2", 1L,
+    1L, "e2", "i2", 2L,
+    2L, "e1", "i1", 1L,
+    2L, "e1", "i1", 2L,
+    2L, "e1", "i1", 3L,
+    2L, "e1", "i1", 4L,
   )
 
   ds <- validate_uniqueness(d)
@@ -30,14 +30,14 @@ test_that("validate_uniqueness -good -all four", {
 test_that("validate_uniqueness -good -events", {
   d <- tibble::tribble(
     ~record_id, ~redcap_event_name,
-    1L,"e1",
-    1L,"e2",
-    1L,"e3",
-    1L,"e4",
-    1L,"e5",
-    2L,"e1",
-    2L,"e2",
-    2L,"e3",
+    1L, "e1",
+    1L, "e2",
+    1L, "e3",
+    1L, "e4",
+    1L, "e5",
+    2L, "e1",
+    2L, "e2",
+    2L, "e3",
   )
 
   ds <- validate_uniqueness(d)
@@ -68,10 +68,10 @@ test_that("validate_uniqueness -good -repeated", {
 test_that("validate_uniqueness -bad -all four", {
   d <- tibble::tribble(
     ~record_id, ~redcap_event_name, ~redcap_repeat_instrument, ~redcap_repeat_instance,
-    1L,"e1", "i1", 1L,
-    1L,"e1", "i1", 2L,
-    1L,"e1", "i1", 3L,
-    1L,"e1", "i1", 3L
+    1L, "e1", "i1", 1L,
+    1L, "e1", "i1", 2L,
+    1L, "e1", "i1", 3L,
+    1L, "e1", "i1", 3L
   )
 
   expect_error(
@@ -88,14 +88,14 @@ test_that("validate_uniqueness -bad -all four", {
 test_that("validate_uniqueness -bad -events", {
   d <- tibble::tribble(
     ~record_id, ~redcap_event_name,
-    1L,"e1",
-    1L,"e2",
-    1L,"e3",
-    1L,"e4",
-    1L,"e5",
-    2L,"e1",
-    2L,"e2",
-    2L,"e1",
+    1L, "e1",
+    1L, "e2",
+    1L, "e3",
+    1L, "e4",
+    1L, "e5",
+    2L, "e1",
+    2L, "e2",
+    2L, "e1",
   )
 
   expect_error(
