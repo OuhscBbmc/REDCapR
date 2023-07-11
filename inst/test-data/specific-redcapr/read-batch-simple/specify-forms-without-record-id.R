ab4c58a889acfd968b1883d676450c3db934ef7a
Will Beasley <wibeasley@hotmail.com>
2023-07-11 16:49:56 -0500

    verify `record_id` IS returned by `redcap_read_oneshot()`
    ref #471

---
+++ inst/test-data/specific-redcapr/read-batch-simple/specify-forms-without-record-id.R
@@ -0,0 +1,9 @@
+structure(list(record_id = c(1, 2, 3, 4, 5), height = c(7, 6, 
+180, 165, 193.04), weight = c(1, 1, 80, 54, 104), bmi = c(204.1, 
+277.8, 24.7, 19.8, 27.9), comments = c("Character in a book, with some guessing", 
+"A mouse character from a good book", "completely made up", "This record doesn't have a DAG assigned\n\nSo call up Trudy on the telephone\nSend her a letter in the mail", 
+"Had a hand for trouble and a eye for cash\n\nHe had a gold watch chain and a black mustache"
+), mugshot = c("mugshot-1.jpg", "mugshot-2.jpg", "mugshot-3.jpg", 
+"mugshot-4.jpg", "mugshot-5.jpg"), health_complete = c(1, 0, 
+2, 2, 0)), row.names = c(NA, -5L), class = c("spec_tbl_df", "tbl_df", 
+"tbl", "data.frame"))
