1417f8e367a29d756473d8626f5194a44b312b6b
Blair Cooper <dev@raincitysolutions.com>
2023-07-12 21:05:02 -0700

    Handle leading space in REDCap checkbox options
    fixes #495
    
    - Modifed pattern_checkboxes regular expression to allow for case where
      there is no space before the pipe character in the Lookbehind and
      Lookahead expressions, and do lazy expansion of the label capture
      group to avoid picking up a space a the end of a label.
    
    - Created test case to verify correct handling when a checkbox option
      has a leading space.

--- R/metadata-utilities.R
+++ R/metadata-utilities.R
@@ -114,7 +114,7 @@ regex_named_captures <- function(pattern, text, perl = TRUE) {
 checkbox_choices <- function(select_choices) {
   checkmate::assert_character(select_choices, any.missing=FALSE, len=1, min.chars=1)
 
-  pattern_checkboxes <- "(?<=\\A| \\| )(?<id>\\d{1,}), (?<label>[^|]{1,})(?= \\| |\\Z)"
+  pattern_checkboxes <- "(?<=\\A| \\| |\\| )(?<id>\\d{1,}), (?<label>[^|]{1,}?)(?= \\| |\\| |\\Z)"
 
   regex_named_captures(pattern = pattern_checkboxes, text = select_choices)
 }
