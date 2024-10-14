420f703eea136dfe81a9a141dcb3aeb4d889d1f4
Will Beasley <wibeasley@hotmail.com>
2024-10-13 21:09:04 -0500

    read-eav-oneshot update
    the attribute order changed for some reason
    
    ref #542

--- inst/test-data/specific-redcapr/read-eav-oneshot/specify-fields.R
+++ inst/test-data/specific-redcapr/read-eav-oneshot/specify-fields.R
@@ -1,15 +1,10 @@
 structure(list(record = c("1", "1", "1", "1", "2", "2", "2", 
 "2", "3", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5"
-), field_name = c("age", "record_id", "name_first", "name_last", 
-"age", "record_id", "name_first", "name_last", "age", "record_id", 
-"name_first", "name_last", "record_id", "age", "name_first", 
-"name_last", "record_id", "age", "name_first", "name_last"), 
-    value = c("11", "1", "Nutmeg", "Nutmouse", "11", "2", "Tumtum", 
-    "Nutmouse", "80", "3", "Marcus", "Wood", "4", "61", "Trudy", 
-    "DAG", "5", "59", "John Lee", "Walker")), row.names = c(NA, 
--20L), spec = structure(list(cols = list(record = structure(list(), class = c("collector_character", 
-"collector")), field_name = structure(list(), class = c("collector_character", 
-"collector")), value = structure(list(), class = c("collector_character", 
-"collector"))), default = structure(list(), class = c("collector_character", 
-"collector")), delim = ","), class = "col_spec"), class = c("spec_tbl_df", 
-"tbl_df", "tbl", "data.frame"))
+), field_name = c("record_id", "name_first", "name_last", "age", 
+"record_id", "name_first", "name_last", "age", "record_id", "name_first", 
+"name_last", "age", "record_id", "name_first", "name_last", "age", 
+"record_id", "name_first", "name_last", "age"), value = c("1", 
+"Nutmeg", "Nutmouse", "11", "2", "Tumtum", "Nutmouse", "11", 
+"3", "Marcus", "Wood", "80", "4", "Trudy", "DAG", "61", "5", 
+"John Lee", "Walker", "59")), row.names = c(NA, -20L), class = c("tbl_df", 
+"tbl", "data.frame"))
