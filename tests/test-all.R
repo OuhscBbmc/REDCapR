f32c3247c3574af5e256c45a22978de19102d2d2
Will Beasley <wibeasley@hotmail.com>
2024-11-05 21:54:33 -0600

    update link of example testing structure
    I think I copied this 10+ years ago?

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,4 +1,4 @@
-# Modeled after the R6 testing structure: https://github.com/wch/R6/blob/master/tests/testthat.R
+# Modeled after the R6 testing structure: https://github.com/r-lib/R6/blob/main/tests/testthat.R
 library(testthat)
 library(REDCapR)
 # source("R/helpers-testing.R")
