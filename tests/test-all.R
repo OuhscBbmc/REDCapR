470c9852d0474a376ddc71a5eee1c77b3932cc57
Will Beasley <wibeasley@hotmail.com>
2024-12-05 10:59:25 -0600

    temporarily switch testing servers
    ref #556

--- tests/test-all.R
+++ tests/test-all.R
@@ -3,13 +3,13 @@ library(testthat)
 library(REDCapR)
 # source("R/helpers-testing.R")
 
-Sys.setenv("redcapr_test_server" = "dev-2")
-message("Using test server '", Sys.getenv("redcapr_test_server"), "'.")
-testthat::test_check("REDCapR")
-
-# Sys.setenv("redcapr_test_server" = "bbmc")
+# Sys.setenv("redcapr_test_server" = "dev-2")
 # message("Using test server '", Sys.getenv("redcapr_test_server"), "'.")
 # testthat::test_check("REDCapR")
+
+Sys.setenv("redcapr_test_server" = "bbmc")
+message("Using test server '", Sys.getenv("redcapr_test_server"), "'.")
+testthat::test_check("REDCapR")
 
 # Sys.setenv("redcapr_test_server" = "coph")
 # message("Using test server '", Sys.getenv("redcapr_test_server"), "'.")
