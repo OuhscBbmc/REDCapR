0917ce4f8a3e262f0e60e4e0fa73509a4c196d6d
Will Beasley <wibeasley@hotmail.com>
2026-01-31 15:39:42 -0600

    reflect more permissions

--- inst/test-data/specific-redcapr/users-export/without-dags--user_form.R
+++ inst/test-data/specific-redcapr/users-export/without-dags--user_form.R
@@ -1,6 +1,6 @@
 structure(list(username = c("unittestphifree", "unittestphifree", 
 "unittestphifree"), form_name = c("demographics", "health", "race_and_ethnicity"
-), permission_id = c(130L, 130L, 130L), permission = structure(c(NA_integer_, 
-NA_integer_, NA_integer_), levels = c("no_access", "readonly", 
-"edit_form", "edit_survey", "unknown"), class = "factor")), row.names = c(NA, 
--3L), class = c("tbl_df", "tbl", "data.frame"))
+), permission_id = c(1L, 1L, 1L), permission = structure(c(3L, 
+3L, 3L), levels = c("no_access", "readonly", "edit_form", "edit_survey", 
+"unknown"), class = "factor")), row.names = c(NA, -3L), class = c("tbl_df", 
+"tbl", "data.frame"))
