b99863530798d329125d35db869c43cb3f4dc0f5
Will Beasley <wibeasley@hotmail.com>
2026-07-26 20:39:02 -0500

    consolidate expected value
    ref #599

--- tests/testthat/test-write-serialization.R
+++ tests/testthat/test-write-serialization.R
@@ -4,10 +4,9 @@ test_that("write serialization preserves large IDs without scientific notation",
     value = c("a", "b", "c", "d")
   )
 
-  csv <- REDCapR:::serialize_csv_for_write(ds)
+  expected  <- "\"record_id\",\"value\"\n99999,\"a\"\n100000,\"b\"\n,\"c\"\n200000,\"d\""
+  observed  <- REDCapR:::serialize_csv_for_write(ds)
 
-  expect_match(csv, "100000,\"b\"", fixed = TRUE)
-  expect_match(csv, ",\"c\"", fixed = TRUE)
-  expect_match(csv, "200000,\"d\"", fixed = TRUE)
-  expect_false(grepl("1e\\+05|2e\\+05", csv))
+  expect_equal(observed, expected)
+  expect_false(grepl("1e\\+05|2e\\+05", observed))
 })
