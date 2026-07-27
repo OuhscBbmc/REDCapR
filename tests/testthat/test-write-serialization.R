f92f5a53e9c67f71efc09f9a486fad359da2df7a
Will Beasley <wibeasley@hotmail.com>
2026-07-26 20:37:20 -0500

    add an NA to the test
    ref #599

--- tests/testthat/test-write-serialization.R
+++ tests/testthat/test-write-serialization.R
@@ -1,12 +1,13 @@
 test_that("write serialization preserves large IDs without scientific notation", {
   ds <- data.frame(
-    record_id = c(99999, 100000, 200000),
-    value = c("a", "b", "c")
+    record_id = c(99999, 100000, NA, 200000),
+    value = c("a", "b", "c", "d")
   )
 
   csv <- REDCapR:::serialize_csv_for_write(ds)
 
   expect_match(csv, "100000,\"b\"", fixed = TRUE)
-  expect_match(csv, "200000,\"c\"", fixed = TRUE)
+  expect_match(csv, ",\"c\"", fixed = TRUE)
+  expect_match(csv, "200000,\"d\"", fixed = TRUE)
   expect_false(grepl("1e\\+05|2e\\+05", csv))
 })
