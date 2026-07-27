9fc321742287b7083916397834a80d389676cb68
Will Beasley <wibeasley@hotmail.com>
2026-07-26 20:36:53 -0500

    change location for the sake of the Rd generation
    ref #599

--- R/redcap-write-oneshot.R
+++ R/redcap-write-oneshot.R
@@ -99,21 +99,6 @@
 #' result_write$raw_text
 #' }
 
-serialize_csv_for_write <- function(ds) {
-  csv_elements <- NULL
-  old_options <- options(scipen = 999)
-  on.exit(options(old_options), add = TRUE)
-
-  con <- base::textConnection(
-    object  = "csv_elements",
-    open    = "w",
-    local   = TRUE
-  )
-  on.exit(close(con), add = TRUE)
-
-  utils::write.csv(ds, con, row.names = FALSE, na = "")
-  paste(csv_elements, collapse = "\n")
-}
 
 #' @importFrom magrittr %>%
 #' @export
@@ -202,4 +187,20 @@ redcap_write_oneshot <- function(
     elapsed_seconds           = kernel$elapsed_seconds,
     raw_text                  = kernel$raw_text
   )
+}
+
+serialize_csv_for_write <- function(ds) {
+  csv_elements <- NULL
+  old_options <- options(scipen = 999)
+  on.exit(options(old_options), add = TRUE)
+
+  con <- base::textConnection(
+    object  = "csv_elements",
+    open    = "w",
+    local   = TRUE
+  )
+  on.exit(close(con), add = TRUE)
+
+  utils::write.csv(ds, con, row.names = FALSE, na = "")
+  paste(csv_elements, collapse = "\n")
 }
