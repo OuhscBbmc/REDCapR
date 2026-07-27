8037c227016fe1dc3413ce9664fa81e9ff16fb2c
Will Beasley <wibeasley@hotmail.com>
2026-07-27 14:43:22 -0500

    goodpractice rec
    > use `expect_s3_class(x, 'class')` instead of `expect_equal(class(x),  'class')`. The dedicated function gives clearer failure messages and  handles multiple classes correctly.
    
    ref #610

--- tests/testthat/test-utilities-replace_nas_with_explicit.R
+++ tests/testthat/test-utilities-replace_nas_with_explicit.R
@@ -28,7 +28,7 @@ test_that("replace_nas_factor_standard", {
 
   a <- REDCapR:::replace_nas_with_explicit(a, create_factor=FALSE, add_unknown_level=TRUE)
   expect_equal(a, expected, label="The correct letters should have been replaced.")
-  expect_equal(class(a), "factor")
+  expect_s3_class(a, "factor")
 })
 
 test_that("replace_nas_factor_create_not_existing", {
