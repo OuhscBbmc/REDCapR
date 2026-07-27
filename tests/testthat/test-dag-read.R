177dc5a227dab341e688b5e143cc88fb612a14e6
Will Beasley <wibeasley@hotmail.com>
2026-07-27 13:53:39 -0500

    goodpractice recs
    > use `!any(x)` instead of `all(!x)`, and `!all(x)` instead of `any(!x)`. Outer negation short-circuits and is clearer.
    
    ref #610

--- tests/testthat/test-dag-read.R
+++ tests/testthat/test-dag-read.R
@@ -37,7 +37,7 @@ test_that("dag-default", {
       verbose     = FALSE
     )
 
-  expect_true(all(!is.na(actual$data$data_access_group_id)))
+  expect_true(!any(is.na(actual$data$data_access_group_id)))
   expect_true(all(0 < actual$data$data_access_group_id))
 
   actual$data$data_access_group_id <- NULL
