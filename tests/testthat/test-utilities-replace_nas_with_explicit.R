61daa4079bcc19911524b0e9f7bc19106fbee14a
Will Beasley <wibeasley@hotmail.com>
2026-07-27 20:32:55 -0500

    goodpractice rec
    >prefer `expect_identical()` over `expect_equal()` when the comparison  should be exact. `expect_equal()` uses `all.equal()` with tolerance,  which can mask subtle type or attribute differences.
    
    ref #610

--- tests/testthat/test-utilities-replace_nas_with_explicit.R
+++ tests/testthat/test-utilities-replace_nas_with_explicit.R
@@ -11,7 +11,7 @@ test_that("replace_nas_character_standard", {
   )
 
   a <- REDCapR:::replace_nas_with_explicit(a)
-  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_identical(a, expected, label="The correct letters should have been replaced.")
   expect_type(a, "character")
 })
 
@@ -27,7 +27,7 @@ test_that("replace_nas_factor_standard", {
   )
 
   a <- REDCapR:::replace_nas_with_explicit(a, create_factor=FALSE, add_unknown_level=TRUE)
-  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_identical(a, expected, label="The correct letters should have been replaced.")
   expect_s3_class(a, "factor")
 })
 
@@ -43,7 +43,7 @@ test_that("replace_nas_factor_create_not_existing", {
   )
 
   a <- REDCapR:::replace_nas_with_explicit(a, create_factor=TRUE, add_unknown_level=TRUE)
-  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_identical(a, expected, label="The correct letters should have been replaced.")
   expect_s3_class(a, "factor")
 })
 
@@ -59,7 +59,7 @@ test_that("replace_nas_factor_create_already_existing", {
   )
 
   a <- REDCapR:::replace_nas_with_explicit(a, create_factor=TRUE, add_unknown_level=TRUE)
-  expect_equal(a, expected, label="The correct letters should have been replaced.")
+  expect_identical(a, expected, label="The correct letters should have been replaced.")
   expect_s3_class(a, "factor")
 })
 
