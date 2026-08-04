ae317fe0969b2318b1e5de5fdcb9ad95ae0194f7
Will Beasley <wibeasley@hotmail.com>
2026-08-04 12:00:40 -0500

    `redcap_event_instruments()` accepts a delimiter
    ref #570

--- R/redcap-event-instruments.R
+++ R/redcap-event-instruments.R
@@ -17,6 +17,17 @@
 #' project.  Required.
 #' @param arms A character string of arms to retrieve.
 #' Defaults to all arms of the project.
+#' @param delimiter A single-character value passed to
+#' the `delim` parameter of [readr::read_delim()].
+#' Options include:
+#' 1. "," (a comma, the default),
+#' 1. ";" (a semi-colon),
+#' 1. "|" (a pipe),
+#' 1. "^" (a caret), or
+#' 1. "tab" (pass "tab", instead of a symbol).
+#'
+#' When "tab" is passed to the REDCapR function,
+#' it is converted to `\t` before passing to [readr::read_delim()].
 #' @param verbose A boolean value indicating if `message`s should be printed
 #' to the R console during the operation.  The verbose output might contain
 #' sensitive information (*e.g.* PHI), so turn this off if the output might
@@ -84,6 +95,7 @@ redcap_event_instruments <- function(
   redcap_uri,
   token,
   arms              = NULL,
+  delimiter         = ",",
   verbose           = TRUE,
   config_options    = NULL,
   handle_httr       = NULL
@@ -91,8 +103,10 @@ redcap_event_instruments <- function(
 
   checkmate::assert_character(redcap_uri, any.missing=FALSE, len=1, pattern="^.{1,}$")
   checkmate::assert_character(token     , any.missing=FALSE, len=1, pattern="^.{1,}$")
+  checkmate::assert_character(delimiter , any.missing=FALSE, len=1, pattern = "^(?:,|;|\\||\\^|tab)$")
 
   token   <- sanitize_token(token)
+  delimiter  <- dplyr::if_else(delimiter == "tab", "\t", delimiter)
   verbose <- verbose_prepare(verbose)
 
   post_body <- list(
@@ -122,9 +136,10 @@ redcap_event_instruments <- function(
       {
         # Convert the raw text to a dataset.
         ds <-
-          readr::read_csv(
+          readr::read_delim(
             file            = I(kernel$raw_text),
-            col_types       = col_types
+            col_types       = col_types,
+            delim           = delimiter
           )
       },
       silent = TRUE
